899b175607a39e44a0d9c411adea5a136c8d85b9
aidevnn <qadassi.ilyas@gmail.com>
2022-10-01 14:45:23 +0300

    Work Table for crafting

--- FastGoat/Program.cs
+++ FastGoat/Program.cs
@@ -2,11 +2,11 @@
 using FastGoat.Gp;
 using FastGoat.UserGroup;
 
-//////////////////////////////////
-//                              //
-//    Work Table for crafting   //
-//                              //
-//////////////////////////////////
+                    //////////////////////////////////
+                    //                              //
+                    //    Work Table for crafting   //
+                    //                              //
+                    //////////////////////////////////
 
 var n = Group.Generate("N", new Zn(9)[1]);
 var g = Group.Generate("G", new Zn(3)[1]);
