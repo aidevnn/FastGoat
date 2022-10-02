7c2ac73427d05fb53598748d75a46eb9dc177d6c
aidevnn <qadassi.ilyas@gmail.com>
2022-10-02 17:57:06 +0300

    Quotient group throw exception unit test

--- Tests/ZnQuotientUnitTest.cs
+++ Tests/ZnQuotientUnitTest.cs
@@ -46,4 +46,25 @@ public class ZnQuotientUnitTest
         var hQuo = h1.Over(h2);
         Assert.True(hQuo.IsIsomorphicTo(klein));
     }
+    
+    [Fact]
+    public void Test3ThrowException()
+    {
+        var z40 = new Zn(40);
+        var g40 = Group.Generate(z40[1]);
+        var g20 = Group.Generate(g40, g40[2]);
+        var g8 = Group.Generate(g40, g40[5]);
+        Assert.Throws<GroupException>(() =>
+        {
+            _ = g20.Over(g8);
+        });
+
+        var s4 = new Sn(4);
+        var g24 = Group.Generate(s4[(1, 2)], s4[(1, 2, 3, 4)]);
+        var g3 = Group.Generate(s4[(1, 2, 3)]);
+        Assert.Throws<GroupException>(() =>
+        {
+            _ = g24.Over(g3);
+        });
+    }
 }
