bad57aedc5fbea97114a69bd8e0326a9457c8646
aidevnn <qadassi.ilyas@gmail.com>
2022-10-04 05:12:03 +0300

    Examples unit tests

---
+++ Tests/ExamplesUnitTest.cs
@@ -0,0 +1,35 @@
+using System.Linq;
+using FastGoat;
+using FastGoat.Examples;
+using FastGoat.Gp;
+using FastGoat.UserGroup;
+using Xunit;
+
+namespace Tests;
+
+public class ExamplesUnitTest
+{
+    [Fact]
+    public void Test1Group21()
+    {
+        GroupOrder21.Zn21();
+        GroupOrder21.ZnDirectProduct();
+        
+        GroupOrder21.Symmetric7();
+        GroupOrder21.Symmetric7Fast();
+        
+        GroupOrder21.SemiDirectProduct();
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void Test1Invariants()
+    {
+        var c14 = new Cn(14);
+        var c21 = new Cn(21);
+        var bg = Product.Group(c14, c21);
+        var g = Group.Create(bg.Name, bg);
+        var decomposition = AbelianInvariantsFactors.Reduce(g);
+        Assert.True(decomposition.SequenceEqual(new[] { 7, 42 }));
+    }
+}
