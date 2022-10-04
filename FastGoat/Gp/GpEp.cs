e7db4932c1fa40a7cf542e605633dc98cb3bdb4f
aidevnn <qadassi.ilyas@gmail.com>
2022-10-04 17:45:01 +0300

    Memory issue. Avoiding recreating the basegroup for each element

--- FastGoat/Gp/GpEp.cs
+++ FastGoat/Gp/GpEp.cs
@@ -18,9 +18,9 @@ public struct Gp2<T1, T2> : IGroup<Ep2<T1, T2>> where T1 : IElt<T1> where T2 : I
 
     public bool Equals(IGroup<Ep2<T1, T2>>? other) => other?.Hash == Hash;
     public int Hash { get; }
-    public Ep2<T1, T2> Neutral() => new(G1.Neutral(), G2.Neutral());
-    public Ep2<T1, T2> Invert(Ep2<T1, T2> e) => new(G1.Invert(e.E1), G2.Invert(e.E2));
-    public Ep2<T1, T2> Op(Ep2<T1, T2> e1, Ep2<T1, T2> e2) => new(G1.Op(e1.E1, e2.E1), G2.Op(e1.E2, e2.E2));
+    public Ep2<T1, T2> Neutral() => new(this, G1.Neutral(), G2.Neutral());
+    public Ep2<T1, T2> Invert(Ep2<T1, T2> e) => new(this, G1.Invert(e.E1), G2.Invert(e.E2));
+    public Ep2<T1, T2> Op(Ep2<T1, T2> e1, Ep2<T1, T2> e2) => new(this, G1.Op(e1.E1, e2.E1), G2.Op(e1.E2, e2.E2));
 
     public Ep2<T1, T2> this[params ValueType[] us]
     {
@@ -51,7 +51,7 @@ public struct Gp2<T1, T2> : IGroup<Ep2<T1, T2>> where T1 : IElt<T1> where T2 : I
     {
         foreach (var e1 in G1)
         foreach (var e2 in G2)
-            yield return new(e1, e2);
+            yield return new(this, e1, e2);
     }
 
     public IEnumerator<Ep2<T1, T2>> GetEnumerator() => GetElements().GetEnumerator();
@@ -71,6 +71,14 @@ public struct Ep2<T1, T2> : IElt<Ep2<T1, T2>> where T1 : IElt<T1> where T2 : IEl
         E2 = e2;
         Hash = (e1.Hash, e2.Hash).GetHashCode();
         Gp2 = new(e1.BaseGroup, e2.BaseGroup);
+    }
+
+    public Ep2(Gp2<T1, T2> gp, T1 e1, T2 e2)
+    {
+        E1 = e1;
+        E2 = e2;
+        Hash = (e1.Hash, e2.Hash).GetHashCode();
+        Gp2 = gp;
     }
 
     public bool Equals(Ep2<T1, T2> other) => other.Hash == Hash;
@@ -121,11 +129,11 @@ public struct Gp3<T1, T2, T3> : IGroup<Ep3<T1, T2, T3>> where T1 : IElt<T1> wher
 
     public bool Equals(IGroup<Ep3<T1, T2, T3>>? other) => other?.Hash == Hash;
     public int Hash { get; }
-    public Ep3<T1, T2, T3> Neutral() => new(G1.Neutral(), G2.Neutral(), G3.Neutral());
-    public Ep3<T1, T2, T3> Invert(Ep3<T1, T2, T3> e) => new(G1.Invert(e.E1), G2.Invert(e.E2), G3.Invert(e.E3));
+    public Ep3<T1, T2, T3> Neutral() => new(this, G1.Neutral(), G2.Neutral(), G3.Neutral());
+    public Ep3<T1, T2, T3> Invert(Ep3<T1, T2, T3> e) => new(this, G1.Invert(e.E1), G2.Invert(e.E2), G3.Invert(e.E3));
 
     public Ep3<T1, T2, T3> Op(Ep3<T1, T2, T3> e1, Ep3<T1, T2, T3> e2) =>
-        new(G1.Op(e1.E1, e2.E1), G2.Op(e1.E2, e2.E2), G3.Op(e1.E3, e2.E3));
+        new(this, G1.Op(e1.E1, e2.E1), G2.Op(e1.E2, e2.E2), G3.Op(e1.E3, e2.E3));
 
     public Ep3<T1, T2, T3> this[params ValueType[] us]
     {
@@ -159,7 +167,7 @@ public struct Gp3<T1, T2, T3> : IGroup<Ep3<T1, T2, T3>> where T1 : IElt<T1> wher
         foreach (var e1 in G1)
         foreach (var e2 in G2)
         foreach (var e3 in G3)
-            yield return new(e1, e2, e3);
+            yield return new(this, e1, e2, e3);
     }
 
     public IEnumerator<Ep3<T1, T2, T3>> GetEnumerator() => GetElements().GetEnumerator();
@@ -181,6 +189,15 @@ public struct Ep3<T1, T2, T3> : IElt<Ep3<T1, T2, T3>> where T1 : IElt<T1> where 
         E3 = e3;
         Hash = (e1.Hash, e2.Hash, e3.Hash).GetHashCode();
         Gp3 = new(e1.BaseGroup, e2.BaseGroup, e3.BaseGroup);
+    }
+
+    public Ep3(Gp3<T1, T2, T3> gp, T1 e1, T2 e2, T3 e3)
+    {
+        E1 = e1;
+        E2 = e2;
+        E3 = e3;
+        Hash = (e1.Hash, e2.Hash, e3.Hash).GetHashCode();
+        Gp3 = gp;
     }
 
     public bool Equals(Ep3<T1, T2, T3> other) => other.Hash == Hash;
@@ -236,12 +253,12 @@ public struct Gp4<T1, T2, T3, T4> : IGroup<Ep4<T1, T2, T3, T4>> where T1 : IElt<
 
     public bool Equals(IGroup<Ep4<T1, T2, T3, T4>>? other) => other?.Hash == Hash;
     public int Hash { get; }
-    public Ep4<T1, T2, T3, T4> Neutral() => new(G1.Neutral(), G2.Neutral(), G3.Neutral(), G4.Neutral());
+    public Ep4<T1, T2, T3, T4> Neutral() => new(this, G1.Neutral(), G2.Neutral(), G3.Neutral(), G4.Neutral());
 
     public Ep4<T1, T2, T3, T4> Invert(Ep4<T1, T2, T3, T4> e) =>
-        new(G1.Invert(e.E1), G2.Invert(e.E2), G3.Invert(e.E3), G4.Invert(e.E4));
+        new(this, G1.Invert(e.E1), G2.Invert(e.E2), G3.Invert(e.E3), G4.Invert(e.E4));
 
-    public Ep4<T1, T2, T3, T4> Op(Ep4<T1, T2, T3, T4> e1, Ep4<T1, T2, T3, T4> e2) => new(G1.Op(e1.E1, e2.E1),
+    public Ep4<T1, T2, T3, T4> Op(Ep4<T1, T2, T3, T4> e1, Ep4<T1, T2, T3, T4> e2) => new(this, G1.Op(e1.E1, e2.E1),
         G2.Op(e1.E2, e2.E2), G3.Op(e1.E3, e2.E3), G4.Op(e1.E4, e2.E4));
 
     public Ep4<T1, T2, T3, T4> this[params ValueType[] us]
@@ -279,7 +296,7 @@ public struct Gp4<T1, T2, T3, T4> : IGroup<Ep4<T1, T2, T3, T4>> where T1 : IElt<
         foreach (var e2 in G2)
         foreach (var e3 in G3)
         foreach (var e4 in G4)
-            yield return new(e1, e2, e3, e4);
+            yield return new(this, e1, e2, e3, e4);
     }
 
     public IEnumerator<Ep4<T1, T2, T3, T4>> GetEnumerator() => GetElements().GetEnumerator();
@@ -306,6 +323,16 @@ public struct Ep4<T1, T2, T3, T4> : IElt<Ep4<T1, T2, T3, T4>> where T1 : IElt<T1
         E4 = e4;
         Hash = (e1.Hash, e2.Hash, e3.Hash, e4.Hash).GetHashCode();
         Gp4 = new(e1.BaseGroup, e2.BaseGroup, e3.BaseGroup, e4.BaseGroup);
+    }
+
+    public Ep4(Gp4<T1, T2, T3, T4> gp, T1 e1, T2 e2, T3 e3, T4 e4)
+    {
+        E1 = e1;
+        E2 = e2;
+        E3 = e3;
+        E4 = e4;
+        Hash = (e1.Hash, e2.Hash, e3.Hash, e4.Hash).GetHashCode();
+        Gp4 = gp;
     }
 
     public bool Equals(Ep4<T1, T2, T3, T4> other) => other.Hash == Hash;
@@ -366,12 +393,12 @@ public struct Gp5<T1, T2, T3, T4, T5> : IGroup<Ep5<T1, T2, T3, T4, T5>> where T1
     public int Hash { get; }
 
     public Ep5<T1, T2, T3, T4, T5> Neutral() =>
-        new(G1.Neutral(), G2.Neutral(), G3.Neutral(), G4.Neutral(), G5.Neutral());
+        new(this, G1.Neutral(), G2.Neutral(), G3.Neutral(), G4.Neutral(), G5.Neutral());
 
-    public Ep5<T1, T2, T3, T4, T5> Invert(Ep5<T1, T2, T3, T4, T5> e) => new(G1.Invert(e.E1), G2.Invert(e.E2),
+    public Ep5<T1, T2, T3, T4, T5> Invert(Ep5<T1, T2, T3, T4, T5> e) => new(this, G1.Invert(e.E1), G2.Invert(e.E2),
         G3.Invert(e.E3), G4.Invert(e.E4), G5.Invert(e.E5));
 
-    public Ep5<T1, T2, T3, T4, T5> Op(Ep5<T1, T2, T3, T4, T5> e1, Ep5<T1, T2, T3, T4, T5> e2) => new(
+    public Ep5<T1, T2, T3, T4, T5> Op(Ep5<T1, T2, T3, T4, T5> e1, Ep5<T1, T2, T3, T4, T5> e2) => new(this,
         G1.Op(e1.E1, e2.E1), G2.Op(e1.E2, e2.E2), G3.Op(e1.E3, e2.E3), G4.Op(e1.E4, e2.E4), G5.Op(e1.E5, e2.E5));
 
     public Ep5<T1, T2, T3, T4, T5> this[params ValueType[] us]
@@ -412,7 +439,7 @@ public struct Gp5<T1, T2, T3, T4, T5> : IGroup<Ep5<T1, T2, T3, T4, T5>> where T1
         foreach (var e3 in G3)
         foreach (var e4 in G4)
         foreach (var e5 in G5)
-            yield return new(e1, e2, e3, e4, e5);
+            yield return new(this, e1, e2, e3, e4, e5);
     }
 
     public IEnumerator<Ep5<T1, T2, T3, T4, T5>> GetEnumerator() => GetElements().GetEnumerator();
@@ -442,6 +469,17 @@ public struct Ep5<T1, T2, T3, T4, T5> : IElt<Ep5<T1, T2, T3, T4, T5>> where T1 :
         E5 = e5;
         Hash = (e1.Hash, e2.Hash, e3.Hash, e4.Hash, e5.Hash).GetHashCode();
         Gp5 = new(e1.BaseGroup, e2.BaseGroup, e3.BaseGroup, e4.BaseGroup, e5.BaseGroup);
+    }
+
+    public Ep5(Gp5<T1, T2, T3, T4, T5> gp, T1 e1, T2 e2, T3 e3, T4 e4, T5 e5)
+    {
+        E1 = e1;
+        E2 = e2;
+        E3 = e3;
+        E4 = e4;
+        E5 = e5;
+        Hash = (e1.Hash, e2.Hash, e3.Hash, e4.Hash, e5.Hash).GetHashCode();
+        Gp5 = gp;
     }
 
     public bool Equals(Ep5<T1, T2, T3, T4, T5> other) => other.Hash == Hash;
@@ -505,13 +543,13 @@ public struct Gp6<T1, T2, T3, T4, T5, T6> : IGroup<Ep6<T1, T2, T3, T4, T5, T6>> 
     public bool Equals(IGroup<Ep6<T1, T2, T3, T4, T5, T6>>? other) => other?.Hash == Hash;
     public int Hash { get; }
 
-    public Ep6<T1, T2, T3, T4, T5, T6> Neutral() => new(G1.Neutral(), G2.Neutral(), G3.Neutral(), G4.Neutral(),
+    public Ep6<T1, T2, T3, T4, T5, T6> Neutral() => new(this, G1.Neutral(), G2.Neutral(), G3.Neutral(), G4.Neutral(),
         G5.Neutral(), G6.Neutral());
 
-    public Ep6<T1, T2, T3, T4, T5, T6> Invert(Ep6<T1, T2, T3, T4, T5, T6> e) => new(G1.Invert(e.E1), G2.Invert(e.E2),
-        G3.Invert(e.E3), G4.Invert(e.E4), G5.Invert(e.E5), G6.Invert(e.E6));
+    public Ep6<T1, T2, T3, T4, T5, T6> Invert(Ep6<T1, T2, T3, T4, T5, T6> e) => new(this, G1.Invert(e.E1),
+        G2.Invert(e.E2), G3.Invert(e.E3), G4.Invert(e.E4), G5.Invert(e.E5), G6.Invert(e.E6));
 
-    public Ep6<T1, T2, T3, T4, T5, T6> Op(Ep6<T1, T2, T3, T4, T5, T6> e1, Ep6<T1, T2, T3, T4, T5, T6> e2) => new(
+    public Ep6<T1, T2, T3, T4, T5, T6> Op(Ep6<T1, T2, T3, T4, T5, T6> e1, Ep6<T1, T2, T3, T4, T5, T6> e2) => new(this,
         G1.Op(e1.E1, e2.E1), G2.Op(e1.E2, e2.E2), G3.Op(e1.E3, e2.E3), G4.Op(e1.E4, e2.E4), G5.Op(e1.E5, e2.E5),
         G6.Op(e1.E6, e2.E6));
 
@@ -556,7 +594,7 @@ public struct Gp6<T1, T2, T3, T4, T5, T6> : IGroup<Ep6<T1, T2, T3, T4, T5, T6>> 
         foreach (var e4 in G4)
         foreach (var e5 in G5)
         foreach (var e6 in G6)
-            yield return new(e1, e2, e3, e4, e5, e6);
+            yield return new(this, e1, e2, e3, e4, e5, e6);
     }
 
     public IEnumerator<Ep6<T1, T2, T3, T4, T5, T6>> GetEnumerator() => GetElements().GetEnumerator();
@@ -589,6 +627,18 @@ public struct Ep6<T1, T2, T3, T4, T5, T6> : IElt<Ep6<T1, T2, T3, T4, T5, T6>> wh
         E6 = e6;
         Hash = (e1.Hash, e2.Hash, e3.Hash, e4.Hash, e5.Hash, e6.Hash).GetHashCode();
         Gp6 = new(e1.BaseGroup, e2.BaseGroup, e3.BaseGroup, e4.BaseGroup, e5.BaseGroup, e6.BaseGroup);
+    }
+
+    public Ep6(Gp6<T1, T2, T3, T4, T5, T6> gp, T1 e1, T2 e2, T3 e3, T4 e4, T5 e5, T6 e6)
+    {
+        E1 = e1;
+        E2 = e2;
+        E3 = e3;
+        E4 = e4;
+        E5 = e5;
+        E6 = e6;
+        Hash = (e1.Hash, e2.Hash, e3.Hash, e4.Hash, e5.Hash, e6.Hash).GetHashCode();
+        Gp6 = gp;
     }
 
     public bool Equals(Ep6<T1, T2, T3, T4, T5, T6> other) => other.Hash == Hash;
