e71fdda7a69be06c766809dbd0bb36a0ae211e41
aidevnn <qadassi.ilyas@gmail.com>
2022-10-05 21:55:12 +0300

    Un automorphism Group of Cn

---
+++ FastGoat/UserGroup/Un.cs
@@ -0,0 +1,26 @@
+using System.CodeDom;
+
+namespace FastGoat.UserGroup;
+
+public class Un : ConcreteGroup<Automorphism<ZnInt>>
+{
+    public Un(int n) : base($"U{n}", Group.Aut(new Cn(n)), true)
+    {
+        var autCn = (AutomorphismGroup<ZnInt>)BaseGroup;
+        var cn = autCn.G;
+        var elements = new List<Automorphism<ZnInt>>();
+        for (int k = 1; k < n; ++k)
+        {
+            if (IntExt.Gcd(k, n) != 1)
+                continue;
+
+            var ak = autCn[(cn[1], cn[k])];
+            elements.Add(ak);
+        }
+
+        Elements = elements.ToHashSet();
+        LongestCycles = Group.LongestCycles(autCn, elements);
+        ElementsOrders = Group.ElementsOrders(LongestCycles);
+        GroupType = Group.IsCommutative(autCn, LongestCycles.Keys) ? GroupType.AbelianGroup : GroupType.NonAbelianGroup;
+    }
+}
