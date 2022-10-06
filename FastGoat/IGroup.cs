9c99d04052bb127526f0d0cf2c874180a7c86962
aidevnn <qadassi.ilyas@gmail.com>
2022-10-06 19:38:40 +0300

    GetGenerators() method in IGroup interface

--- FastGoat/IGroup.cs
+++ FastGoat/IGroup.cs
@@ -6,6 +6,7 @@ public interface IGroup<T> : IEnumerable<T>, IEquatable<IGroup<T>> where T : IEl
     string Name { get; }
     T this[params ValueType[] us] { get; }
     IEnumerable<T> GetElements();
+    IEnumerable<T> GetGenerators();
     T Neutral();
     T Invert(T e);
     T Op(T e1, T e2);
