a2e0902581dc1b469dfcec001fe3929232e93475
aidevnn <qadassi.ilyas@gmail.com>
2022-10-08 04:46:09 +0300

    All Isomorphics method

--- FastGoat/Examples/GroupOrder32C4C8.cs
+++ FastGoat/Examples/GroupOrder32C4C8.cs
@@ -6,11 +6,12 @@ namespace FastGoat.Examples;
 
 public static class GroupOrder32C4C8
 {
+    private static Cn C3 { get; } = new Cn(3);
     private static Cn C4 { get; } = new Cn(4);
     private static Un U4 { get; } = new Un(4);
     private static Cn C8 { get; } = new Cn(8);
     private static Un U8 { get; } = new Un(8);
-
+    private static Cn C12 { get; } = new Cn(12);
     public static void NonIsomorphic()
     {
         var yC8C4 = U8[(C8[1], C8[5])];
@@ -23,14 +24,30 @@ public static class GroupOrder32C4C8
         var yC4C8 = U4[(C4[1], C4[3])];
         var mapC4C8 = Group.PartialMap((C8[1], yC4C8));
         var thetaC4C8 = Group.HomomorphismMap(C8, U4, mapC4C8);
-        var gC4C8 = Group.SemiDirectProd(C4, thetaC4C8, C8);
-        DisplayGroup.HeadSdp(gC4C8);
-        Console.WriteLine("Sorted Orders {0} : ({1})", gC4C8, gC4C8.ElementsOrders.Values.Ascending().Glue(", "));
+        var gC3C4 = Group.SemiDirectProd(C4, thetaC4C8, C8);
+        DisplayGroup.HeadSdp(gC3C4);
+        Console.WriteLine("Sorted Orders {0} : ({1})", gC3C4, gC3C4.ElementsOrders.Values.Ascending().Glue(", "));
 
         Console.WriteLine();
-        Console.WriteLine("Isomorphic {0} {1} {2}", gC4C8, gC8C4, gC4C8.IsIsomorphicTo(gC8C4));
+        Console.WriteLine("Isomorphic {0} ~ {1} {2}", gC3C4, gC8C4, gC3C4.IsIsomorphicTo(gC8C4));
 
-        var homs = Group.AllHomomorphisms(gC8C4, gC4C8, true);
+        var homs = Group.AllHomomorphisms(gC8C4, gC3C4, true);
+        Console.WriteLine("Homomorphisms Count = {0}", homs.Count);
+        Console.WriteLine("Isomorphisms  Count = {0}", homs.Count(h => h.Values.Distinct().Count() == h.Count));
+    }
+
+    public static void Isomorphic()
+    {
+        var gC3C4 = Product.Generate(C3, C4);
+        DisplayGroup.Head(C12);
+        Console.WriteLine("Sorted Orders {0} : ({1})", C12, C12.ElementsOrders.Values.Ascending().Glue(", "));
+        DisplayGroup.Head(gC3C4);
+        Console.WriteLine("Sorted Orders {0} : ({1})", gC3C4, gC3C4.ElementsOrders.Values.Ascending().Glue(", "));
+
+        Console.WriteLine();
+        Console.WriteLine("Isomorphic {0} ~ {1} {2}", gC3C4, C12, gC3C4.IsIsomorphicTo(C12));
+
+        var homs = Group.AllHomomorphisms(C12, gC3C4, true);
         Console.WriteLine("Homomorphisms Count = {0}", homs.Count);
         Console.WriteLine("Isomorphisms  Count = {0}", homs.Count(h => h.Values.Distinct().Count() == h.Count));
     }
