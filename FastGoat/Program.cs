c6723e4945aa73924129e352d29f0b2dba3e1820
aidevnn <qadassi.ilyas@gmail.com>
2022-10-12 01:48:35 +0300

    Comparing D12 from Symm5 and Symm6

--- FastGoat/Program.cs
+++ FastGoat/Program.cs
@@ -26,7 +26,7 @@ Console.WriteLine("Hello World");
     var d12 = Group.SemiDirectProd(c6, c2);
     var d12b = Group.Generate(s5, s5[(1, 4, 5)], s5[(2, 3)], s5[(4, 5)]);
     var d12c = Group.Generate(s6, s6[(1, 2, 3, 4, 5, 6)], s6[(1, 6), (2, 5), (3, 4)]);
-    
+
     DisplayGroup.HeadElements(a4);
     DisplayGroup.HeadElementsSdp(c3c4);
     DisplayGroup.HeadElementsSdp(d12);
@@ -38,5 +38,17 @@ Console.WriteLine("Hello World");
     Console.WriteLine(c3c4.IsIsomorphicTo(d12b));
     Console.WriteLine(d12.IsIsomorphicTo(d12b));
     Console.WriteLine(d12c.IsIsomorphicTo(d12b));
-    
+
+    var allIsos = Group.AllIsomorphisms(d12b, d12c);
+    Console.WriteLine();
+    Console.WriteLine("Isomorsphims between D12 from Symm5 and Symm6. Total : {0}", allIsos.Count);
+    foreach (var iso in allIsos)
+    {
+        foreach (var kp in iso.OrderBy(kp => d12b.ElementsOrders[kp.Key]))
+        {
+            Console.WriteLine($"{kp.Key,20} => {kp.Value}");
+        }
+
+        Console.WriteLine();
+    }
 }
