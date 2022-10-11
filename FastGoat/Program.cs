6098257352585b09cd5910d20a29716709ca07a1
aidevnn <qadassi.ilyas@gmail.com>
2022-10-12 01:37:54 +0300

    Comparing D12 from Symm5 and Symm6

--- FastGoat/Program.cs
+++ FastGoat/Program.cs
@@ -20,19 +20,23 @@ Console.WriteLine("Hello World");
     var c6 = new Cn(6);
     var s4 = new Symm(4);
     var s5 = new Symm(5);
+    var s6 = new Symm(6);
     var a4 = Group.Generate("A4", s4[(1, 2, 3)], s4[(1, 2), (3, 4)]);
     var c3c4 = Group.SemiDirectProd(c3, c4);
     var d12 = Group.SemiDirectProd(c6, c2);
     var d12b = Group.Generate(s5, s5[(1, 4, 5)], s5[(2, 3)], s5[(4, 5)]);
+    var d12c = Group.Generate(s6, s6[(1, 2, 3, 4, 5, 6)], s6[(1, 6), (2, 5), (3, 4)]);
     
     DisplayGroup.HeadElements(a4);
-    DisplayGroup.HeadElements(d12b);
     DisplayGroup.HeadElementsSdp(c3c4);
     DisplayGroup.HeadElementsSdp(d12);
+    DisplayGroup.HeadElements(d12b);
+    DisplayGroup.HeadElements(d12c);
 
     Console.WriteLine(a4.IsIsomorphicTo(c3c4));
     Console.WriteLine(a4.IsIsomorphicTo(d12));
     Console.WriteLine(c3c4.IsIsomorphicTo(d12b));
     Console.WriteLine(d12.IsIsomorphicTo(d12b));
+    Console.WriteLine(d12c.IsIsomorphicTo(d12b));
     
 }
