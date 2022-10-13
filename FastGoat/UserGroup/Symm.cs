63490bebd17b8f5ba5bfe34d1bc6df333378e3dd
aidevnn <qadassi.ilyas@gmail.com>
2022-10-13 21:30:25 +0300

    Symmetric Subgroups hell

--- FastGoat/UserGroup/Symm.cs
+++ FastGoat/UserGroup/Symm.cs
@@ -16,6 +16,9 @@ public class Symm : ConcreteGroup<Perm>
         if (N >= 3)
         {
             yield return new[] { this[(1, 2, 3)] }; // C3
+            
+            // Symmetric or Alternate 
+            yield return new[] { this[(1, 2)], this[(1, 2, 3)] }; // S3
         }
 
         if (N >= 4)
@@ -26,9 +29,9 @@ public class Symm : ConcreteGroup<Perm>
             // Semi-direct Product
             yield return new[] { this[(1, 4), (2, 3)], this[(1, 2, 3, 4)] }; // D8
 
-            // Symmetric 
-            yield return new[] { this[(1, 2)], this[(1, 2, 3)] }; // S3
+            // Symmetric or Alternate 
             yield return new[] { this[(1, 2), (3, 4)], this[(1, 2, 3)] }; // A4
+            yield return new[] { this[(1, 2)], this[(1, 2, 3, 4)] }; // S4
         }
 
         if (N >= 5)
@@ -42,8 +45,8 @@ public class Symm : ConcreteGroup<Perm>
             yield return new[] { this[(2, 3, 5, 4)], this[(1, 2, 3, 4, 5)] }; // C5 : C4
 
             // Symmetric or Alternate 
-            yield return new[] { this[(1, 2)], this[(1, 2, 3, 4)] }; // S4
             yield return new[] { this[(1, 2, 3)], this[(1, 2, 3, 4, 5)] }; // A5
+            yield return new[] { this[(1, 2)], this[(1, 2, 3, 4, 5)] }; // S5
         }
 
         if (N >= 6)
@@ -51,10 +54,10 @@ public class Symm : ConcreteGroup<Perm>
             // Direct Product
             yield return new[] { this[(1, 2), (3, 5)], this[(3, 4, 5, 6)] }; // C8
             yield return new[] { this[(1, 2, 3)], this[(4, 5, 6)] }; // C3 x C3
-            // yield return new[] { this[(1, 2)], this[(3, 4)], this[(5, 6)] }; // C2 x C2 x C2
-            // yield return new[] { this[(1, 2), (3, 4)], this[(1, 3), (2, 4)], this[(1, 2), (5, 6)] }; // V x C2
-            // yield return new[] { this[(1, 2)], this[(1, 2, 3)], this[(1, 2), (4, 5, 6)] }; // S3 x C3
-            // yield return new[] { this[(1, 2)], this[(4, 5)], this[(1, 2, 3)], this[(4, 5, 6)] }; // S3 x S3
+            yield return new[] { this[(1, 2)], this[(3, 4)], this[(5, 6)] }; // C2 x C2 x C2
+            yield return new[] { this[(1, 2), (3, 4)], this[(1, 3), (2, 4)], this[(1, 2), (5, 6)] }; // V x C2
+            yield return new[] { this[(1, 2)], this[(1, 2, 3)], this[(1, 2), (4, 5, 6)] }; // S3 x C3
+            yield return new[] { this[(1, 2)], this[(4, 5)], this[(1, 2, 3)], this[(4, 5, 6)] }; // S3 x S3
 
             // Semi-direct Product 
             
@@ -66,8 +69,7 @@ public class Symm : ConcreteGroup<Perm>
 
 
             // Symmetric or Alternate 
-            // yield return new[] { this[(1, 2)], this[(1, 2, 3, 4, 5)] }; // S5
-            // yield return new[] { this[(4, 5, 6)], this[(1, 2, 3, 4, 5)] }; // A6
+            yield return new[] { this[(4, 5, 6)], this[(1, 2, 3, 4, 5)] }; // A6
         }
 
         if (N >= 7)
