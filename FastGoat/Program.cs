517022d550a774fd982e0facc20b06a0026dc387
aidevnn <qadassi.ilyas@gmail.com>
2022-10-19 15:39:00 +0300

    Clean crafting table

--- FastGoat/Program.cs
+++ FastGoat/Program.cs
@@ -13,32 +13,3 @@ using static FastGoat.EnumerableExt;
 //////////////////////////////////
 
 Console.WriteLine("Hello World");
-
-IEnumerable<IEnumerable<int>> YieldAllPermutationsOld(int n)
-{
-    if (n == 0)
-        yield return Enumerable.Empty<int>();
-    else
-        foreach (var perm in YieldAllPermutations(n - 1))
-            for (int i = 0; i < n; i++)
-                yield return perm.Take(i).Append(n).Concat(perm.Skip(i));
-}
-
-{
-    for (int i = 0; i < 5; i++)
-    {
-        
-        {
-            GlobalStopWatch.Restart();
-            Console.WriteLine(YieldAllPermutations(9).Count());
-            GlobalStopWatch.Show("Yield New");;
-        }
-
-        {
-            GlobalStopWatch.Restart();
-            Console.WriteLine(YieldAllPermutationsOld(9).Count());
-            GlobalStopWatch.Show("Yield Old");
-        }
-
-    }
-}
