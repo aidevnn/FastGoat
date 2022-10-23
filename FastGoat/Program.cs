8ca7728c62a92bf12395ca995a8e0b5de64da58e
aidevnn <qadassi.ilyas@gmail.com>
2022-10-24 00:06:58 +0300

    Quartenion in GL(2, 3)

--- FastGoat/Program.cs
+++ FastGoat/Program.cs
@@ -15,3 +15,14 @@ using static FastGoat.EnumerableExt;
 //////////////////////////////////
 
 Console.WriteLine("Hello World");
+
+{
+    var gl = new GL(2, 3);
+    var a = gl[2, 2, 2, 1];
+    var b = gl[0, 2, 1, 0];
+    var q8 = Group.Generate("Q8", gl, a, b);
+    DisplayGroup.HeadElements(q8);
+
+    var wgQ8 = new WordGroup("wgQ8", "a2=b2, a-1=bab-1");
+    Console.WriteLine(wgQ8.IsIsomorphicTo(q8));
+}
