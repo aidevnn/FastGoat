0e616620c912a1f1e1314804f716570248edecd7
aidevnn <qadassi.ilyas@gmail.com>
2022-11-06 23:49:06 +0300

    Conway Polynomials example, bug

--- FastGoat/Examples/ConwayPolynoms.cs
+++ FastGoat/Examples/ConwayPolynoms.cs
@@ -73,11 +73,13 @@ public static class ConwayPolynoms
         Console.WriteLine();
     }
 
-    public static void Automorphism(int p, int n)
+    public static void AutomorphismFromPoly(int p, int n, bool verbose = true)
     {
         var cnPoly = GetPoly(p, n);
+        Console.WriteLine($"Poly p={p} n={n} : {cnPoly}");
         var x = FpPolynom.Fpx(p);
         var q = (int)Math.Pow(p, n);
+        var n1 = q - 1;
         var cn = new Cn(p);
         var gr = Product.GpGenerate(Enumerable.Repeat(cn, n).Cast<IGroup<ZnInt>>().ToArray());
 
@@ -86,12 +88,75 @@ public static class ConwayPolynoms
         var listEp = list.Select(c => Product.Ep(c.Select(i => cn[i]).ToArray())).ToArray();
         var pmap = listEp.SkipLast(1).Select((e, i) => (e, listEp[(i + 1) % q])).ToDictionary(e => e.e, e => e.Item2);
         
-        var aut = Group.AutomorphismMap(gr, pmap);
-        var autFq = Group.AutBase(gr);
-        var fq = Group.Generate($"F{q}", autFq, autFq.Create(aut));
-        DisplayGroup.HeadOrders(fq);
-        Console.WriteLine(aut.GlueMap("\n"));
-        Console.WriteLine();
+        var fbMap = Group.AutomorphismMap(gr, pmap);
+        var aut = Group.AutBase(gr);
+        var fb = aut.Create(fbMap);
+
+        var fbCycle = Group.Cycle(aut, fb);
+        var zero = gr.Neutral();
+        var one = gr.GetGenerators().Ascending().First();
+        var elt2pow = fbCycle.ToDictionary(e => e.Key[one], e => e.Value);
+        var pow2elt = elt2pow.ToDictionary(e => e.Value, e => e.Key);
+
+        Ep<ZnInt> Mul(Ep<ZnInt> a1, Ep<ZnInt> a2)
+        {
+            if (a1.Equals(zero) || a2.Equals(zero))
+                return zero;
+
+            var i = elt2pow[a1];
+            var j = elt2pow[a2];
+
+            // return aut.Times(fb, i + j)[one];
+            var k = (i + j) % n1;
+            return pow2elt[k == 0 ? n1 : k];
+        }
+
+        bool CheckDistributivity(Ep<ZnInt> m, Ep<ZnInt> a1, Ep<ZnInt> a2)
+        {
+            var e0 = Mul(m, gr.Op(a1, a2));
+            var e1 = gr.Op(Mul(m, a1), Mul(m, a2));
+            return e0.Equals(e1);
+        }
+
+        bool CheckFpSpace(int k, Ep<ZnInt> a1, Ep<ZnInt> a2)
+        {
+            var ka1 = gr.Times(a1, k);
+            var ka2 = gr.Times(a2, k);
+            var e0 = Mul(a1, a2);
+            var ke0 = gr.Times(e0, k);
+            var e1 = Mul(a1, ka2);
+            var e2 = Mul(ka1, a2);
+            return ke0.Equals(e1) && ke0.Equals(e2);
+        }
+
+        Console.WriteLine("Multiplication Table of F{0}", gr.Count());
+        var distrib = true;
+        var fpspace = true;
+        var fps = Enumerable.Range(0, p).ToArray();
+        foreach (var e1 in gr)
+        {
+            foreach (var e2 in gr)
+            {
+                var e3 = Mul(e1, e2);
+                if (distrib)
+                    distrib &= gr.All(m => CheckDistributivity(m, e1, e2));
+
+                if (fpspace)
+                    fpspace &= fps.All(k => CheckFpSpace(k, e1, e2));
+
+                if (verbose)
+                    Console.WriteLine("{0} x {1} = {2}", e1, e2, e3);
+            }
+
+            if (verbose)
+                Console.WriteLine();
+        }
+
+        Console.WriteLine("Check Distributivity {0}", distrib ? "Pass" : "Fail");
+        Console.WriteLine("Check Fp-Space       {0}", fpspace ? "Pass" : "Fail");
+
+        if (verbose)
+            Console.WriteLine();
     }
 
     public static void Run()
@@ -121,7 +186,29 @@ public static class ConwayPolynoms
 
     public static void FastAutomorphism()
     {
-        Automorphism(2, 3);
-        Automorphism(2, 6);
+        AutomorphismFromPoly(2, 1);
+        AutomorphismFromPoly(3, 1);
+        AutomorphismFromPoly(5, 1);
+        AutomorphismFromPoly(7, 1);
+
+        AutomorphismFromPoly(2, 2);
+        AutomorphismFromPoly(3, 2);
+
+        AutomorphismFromPoly(2, 3);
+        AutomorphismFromPoly(3, 3);
+    }
+
+    public static void Bench()
+    {
+        
+        GlobalStopWatch.Time("F8", () => AutomorphismFromPoly(2, 3, verbose: false));
+        GlobalStopWatch.Time("F9", () => AutomorphismFromPoly(3, 2, verbose: false));
+        GlobalStopWatch.Time("F25", () => AutomorphismFromPoly(5, 2, verbose: false));
+        GlobalStopWatch.Time("F27", () => AutomorphismFromPoly(3, 3, verbose: false));
+        GlobalStopWatch.Time("F16", () => AutomorphismFromPoly(2, 4, verbose: false));
+        GlobalStopWatch.Time("F125", () => AutomorphismFromPoly(5, 3, verbose: false));
+        GlobalStopWatch.Time("F81", () => AutomorphismFromPoly(3, 4, verbose: false));
+        GlobalStopWatch.Time("F32", () => AutomorphismFromPoly(2, 5, verbose: false));
+
     }
 }
