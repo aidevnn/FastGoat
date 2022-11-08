ebadfc62cdc663f4d49d375f87fa0422ea5bf39f
aidevnn <qadassi.ilyas@gmail.com>
2022-11-08 07:15:06 +0300

    Q(i)

--- FastGoat/Program.cs
+++ FastGoat/Program.cs
@@ -4,6 +4,7 @@ using FastGoat.Commons;
 using FastGoat.Examples;
 using FastGoat.Structures;
 using FastGoat.Structures.CartesianProduct;
+using FastGoat.Structures.VecSpace;
 using FastGoat.UserGroup.Integers;
 using FastGoat.UserGroup.Perms;
 using FastGoat.UserGroup.Polynoms;
@@ -18,3 +19,15 @@ using static FastGoat.Commons.EnumerableExt;
 //////////////////////////////////
 
 Console.WriteLine("Hello World");
+
+{
+    var a = Ring.QPolynomial('a');
+    var p = a.Pow(2) + 1;
+    Console.WriteLine(p);
+
+    var (x, a1, k) = Ring.ExtPolynomial(p, 'x');
+    var x_ai = Enumerable.Range(0, k + 1).Select(e => x - a1.Pow(e)).ToArray();
+    Console.WriteLine(x_ai.Glue("; "));
+
+    Console.WriteLine(x_ai.Aggregate((xi, xj) => xi * xj));
+}
