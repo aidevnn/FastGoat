bac9f317886271ca9dd53b20d9cc547b26f46737
aidevnn <qadassi.ilyas@gmail.com>
2022-11-08 07:14:03 +0300

    Polynomial Ring

---
+++ FastGoat/Structures/Ring.cs
@@ -0,0 +1,69 @@
+using FastGoat.Commons;
+using FastGoat.Structures.VecSpace;
+using FastGoat.UserGroup.Integers;
+
+namespace FastGoat.Structures;
+
+public static class Ring
+{
+    public static IEnumerable<T> TrimSeq<T>(this IEnumerable<T> seq) where T : struct, IElt<T>, IRingElt<T>
+    {
+        var stack = new Stack<T>(seq);
+        while (stack.Count!=0 && stack.Peek().IsZero())
+            stack.Pop();
+
+        return stack.Reverse();
+    }
+    
+    public static T Gcd<T>(T a, T b) where T : struct, IElt<T>, IRingElt<T>
+    {
+        if (b.IsZero())
+            return a.CompareTo(a.Opp()) == -1 ? a.Opp() : a;
+
+        return Gcd(b, a.Div(b).rem);
+    }
+
+    public static (T x, T y) Bezout<T>(T a, T b) where T : struct, IElt<T>, IRingElt<T>
+    {
+        if (b.IsZero())
+            return a.CompareTo(a.Opp()) == -1 ? (a.One.Opp(), a.Zero) : (a.One, a.Zero);
+
+        var (q, r) = a.Div(b);
+        var (x0, y0) = Bezout<T>(b, r);
+        return (y0, x0.Add(q.Mul(y0).Opp()));
+    }
+    
+    public static KPoly<ZnInt> ZPolynomial(int p = 0, char x = 'x')
+    {
+        if (p != 0 && !IntExt.Primes10000.Contains(p))
+            throw new GroupException(GroupExceptionType.GroupDef);
+
+        var kZero = new ZnInt(p, 0);
+        return new KPoly<ZnInt>(x, kZero, new[] { kZero, kZero.One });
+    }
+
+    public static KPoly<Rational> QPolynomial(char x = 'x')
+    {
+        var kZero = new Rational(0, 1);
+        return new KPoly<Rational>(x, kZero, new[] { kZero, kZero.One });
+    }
+
+    public static (KPoly<EPoly<T>>, EPoly<T>, int k) ExtPolynomial<T>(KPoly<T> f, char x) where T : struct, IElt<T>, IRingElt<T>, IFieldElt<T>
+    {
+        var fx = new EPoly<T>(f);
+        var kx = new KPoly<EPoly<T>>(x, fx.Zero, new[] { fx.Zero, fx.One });
+
+        var a = kx.Zero + f.X;
+        var p = 0;
+        var a0 = a.One;
+        for (;;++p)
+        {
+            a0 *= a;
+            if(a0.Equals(a.One))
+                break;
+        }
+
+        return (kx, a[0], p);
+    }
+
+}
---
+++ FastGoat/Structures/VecSpace/EPoly.cs
@@ -0,0 +1,79 @@
+namespace FastGoat.Structures.VecSpace;
+
+public readonly struct EPoly<K> : IVsElt<K, EPoly<K>>, IElt<EPoly<K>>, IRingElt<EPoly<K>>, IFieldElt<EPoly<K>>
+    where K : struct, IElt<K>, IRingElt<K>, IFieldElt<K>
+{
+    public KPoly<K> F { get; }
+    public KPoly<K> Poly { get; }
+
+    public EPoly(KPoly<K> f)
+    {
+        // if (f.P == 0)
+        //     throw new GroupException(GroupExceptionType.GroupDef);
+
+        F = f;
+        Poly = f.X;
+        Hash = (Poly.Hash, f.Hash).GetHashCode();
+    }
+
+    private EPoly(KPoly<K> f, KPoly<K> poly)
+    {
+        F = f;
+        Poly = poly;
+        Hash = (Poly.Hash, f.Hash).GetHashCode();
+    }
+
+    public bool Equals(EPoly<K> other) => Hash == other.Hash;
+
+    public int CompareTo(EPoly<K> other) => Poly.CompareTo(other.Poly);
+
+    public int P => F.P;
+    public EPoly<K> Inv()
+    {
+        var (x, y) = Ring.Bezout(Poly, F);
+        var gcd = (Poly * x + F * y).Div(F).rem;
+        return new(F, (x / gcd).Div(F).rem);
+    }
+
+    public EPoly<K> KMul(K k) => new(F, Poly.KMul(k));
+
+    public int Hash { get; }
+    public bool IsZero() => Poly.IsZero();
+
+    public EPoly<K> Zero => new(F, F.Zero);
+    public EPoly<K> One => new(F, F.One);
+    public EPoly<K> Add(EPoly<K> e)
+    {
+        return new(F, Poly.Add(e.Poly).Div(F).rem);
+    }
+
+    public EPoly<K> Sub(EPoly<K> e)
+    {
+        return new(F, Poly.Sub(e.Poly).Div(F).rem);
+    }
+
+    public EPoly<K> Opp() => new(F, Poly.Opp());
+
+    public EPoly<K> Mul(EPoly<K> e) => new(F, Poly.Mul(e.Poly).Div(F).rem);
+
+    public (EPoly<K> quo, EPoly<K> rem) Div(EPoly<K> e)
+    {
+        var (q, r) = Poly.Div(e.Poly);
+        return (new(F, q.Div(F).rem), new(F, r.Div(F).rem));
+    }
+
+    public EPoly<K> Mul(int k) => new(F, Poly.Mul(k));
+
+    public EPoly<K> Pow(int k) => new(F, Poly.Pow(k).Div(F).rem);
+
+    public override string ToString()
+    {
+        if (IsZero())
+            return "0";
+        
+        var s = $"{Poly}";
+        return s.Contains('+') ? $"({s})" : s;
+    }
+
+    public static implicit operator EPoly<K>(KPoly<K> e) => new(e.One, e);
+}
