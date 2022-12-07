ee04cae0f37146c09cbd365b9c2b00c051f3ad24
aidevnn <qadassi.ilyas@gmail.com>
2022-12-07 19:08:32 +0300

    Gram-Schmidt examples

--- FastGoat/Program.cs
+++ FastGoat/Program.cs
@@ -26,3 +26,74 @@ using System.Threading.Channels;
 //////////////////////////////////
 
 Console.WriteLine("Hello World");
+Random rnd = new Random();
+
+{
+    var m = Ring.Matrix(2, Rational.KZero(), 3, 2, 1, 2);
+    var A = new KMatrix<Rational>(m);
+    var (O, U) = Ring.GramSchmidt(A);
+    Console.WriteLine("Matrix A");
+    Console.WriteLine(A);
+    Console.WriteLine("Matrix O");
+    Console.WriteLine(O);
+    Console.WriteLine("Matrix U");
+    Console.WriteLine(U);
+    Console.WriteLine("(U * O.T).T");
+    Console.WriteLine((U * O.T).T);
+}
+
+{
+    var m = Ring.Matrix(2, Rational.KZero(), 6, 8, 4, 4);
+    var A = new KMatrix<Rational>(m);
+    var (O, U) = Ring.GramSchmidt(A);
+    Console.WriteLine("Matrix A");
+    Console.WriteLine(A);
+    Console.WriteLine("Matrix O");
+    Console.WriteLine(O);
+    Console.WriteLine("Matrix U");
+    Console.WriteLine(U);
+    Console.WriteLine("(U * O.T).T");
+    Console.WriteLine((U * O.T).T);
+}
+
+{
+    var n = 3;
+    var m = Ring.Matrix(n, Rational.KZero(), 1, 2, 3, 4, 7, 6, 2, 1, 4);
+    var A = new KMatrix<Rational>(m);
+    var (O, U) = Ring.GramSchmidt(A);
+    Console.WriteLine("Matrix A");
+    Console.WriteLine(A);
+    Console.WriteLine("Matrix O");
+    Console.WriteLine(O);
+    Console.WriteLine("Matrix U");
+    Console.WriteLine(U);
+    Console.WriteLine("(U * O.T).T");
+    Console.WriteLine((U * O.T).T);
+}
+
+KMatrix<Rational> RandMatrixRational(int n)
+{
+    var m = new KMatrix<Rational>(Rational.KZero(), n, n);
+    while (true)
+    {
+        var m0 = Ring.Matrix(n, Rational.KZero(), (n * n).Range().Select(i => rnd.Next(n + 1)).ToArray());
+        m = new(m0);
+        if (!m.Det.IsZero())
+            return m;
+    }
+}
+
+for (int k = 0; k < 15; ++k)
+{
+    var n = 2 + rnd.Next(15);
+    GlobalStopWatch.Restart();
+    var A = RandMatrixRational(n);
+    var (O, U) = Ring.GramSchmidt(A);
+    Console.WriteLine($"Dim {A.Dim}");
+    Console.WriteLine("U * O.T = A.T : {0}", (U * O.T).Equals(A.T));
+    Console.WriteLine("All Orthogonals : {0}", n.Range().Grid2D(n.Range())
+        .All(e => e.t1.Equals(e.t2) || (O.GetCol(e.t1).T * O.GetCol(e.t2)).IsZero()));
+
+    GlobalStopWatch.Show("GS2");
+    Console.WriteLine();
+}
