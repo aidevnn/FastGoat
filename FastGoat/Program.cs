61d541db24c46ba4890a45189ca21dfb5c136bed
aidevnn <qadassi.ilyas@gmail.com>
2022-12-20 01:51:40 +0300

    PadicGAP and PadicZealous implementation Unit tests

--- FastGoat/Program.cs
+++ FastGoat/Program.cs
@@ -1,4 +1,5 @@
-﻿using FastGoat.Commons;
+﻿using System.Globalization;
+using FastGoat.Commons;
 using FastGoat.Examples;
 using FastGoat.Structures;
 using FastGoat.Structures.CartesianProduct;
@@ -14,6 +15,7 @@ using FastGoat.UserGroup.Words.ToddCoxeter;
 using static FastGoat.Commons.IntExt;
 using static FastGoat.Commons.EnumerableExt;
 using System.Numerics;
+using System.Xml;
 
 //////////////////////////////////
 //                              //
@@ -24,65 +26,126 @@ using System.Numerics;
 Console.WriteLine("Hello World");
 
 {
-    Console.WriteLine("Inverse floating padic");
-    {
-        var a = Padic.Convert(2, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1);
-        var a0 = a.Add(a.Ppow(2));
-        Console.WriteLine($"c = {a}");
-        Console.WriteLine($"a0 = {a0}");
-        for (int i = 0; i < 7; i++)
-        {
-            Console.WriteLine($"Step {i} : {a0}");
-            a0 = a0.Mul(2).Sub(a.Mul(a0.Pow(2)));
-        }
-        Console.WriteLine($"Check a^-1 = {a.Inv()} => {a0.Inv().Equals(a)}");
-    }
+     Console.WriteLine("Qp Zealous P-Adic Numbers");
+     Console.WriteLine(new PadicZealous(5, 3));
+     Console.WriteLine();
+
+     Console.WriteLine("Serie a");
+     var a1 = new PadicZealous(5, 17, 86);
+     Console.WriteLine(a1);
+     Console.WriteLine(a1.Inv());
+     Console.WriteLine(a1.Inv() * a1);
+     Console.WriteLine(a1 * 5.Pow(2));
+     Console.WriteLine(a1 / 5.Pow(2));
+     Console.WriteLine(a1 * 5.Pow(5)); // one incorrect digit, 86~O(5^2) and 5^5~O(5^5) and the limit is O(5^7)
+     Console.WriteLine(a1 / 5.Pow(5)); // one incorrect digit
+     var a2 = new PadicZealous(5, 3, 75);
+     Console.WriteLine(a2);
+     Console.WriteLine(a2 * 5);
+     Console.WriteLine(a2 / 5);
+     var a3 = new PadicZealous(5, 3, 15);
+     Console.WriteLine(a3);
+     Console.WriteLine(a3 * 5);
+     Console.WriteLine(a3 / 5);
+     Console.WriteLine();
+
+     Console.WriteLine("Serie b");
+     var b1 = new PadicZealous(5, 3, 171);
+     var b2 = new PadicZealous(5, 3, -171);
+     var b3 = new PadicZealous(5, 6, 171);
+     var b4 = new PadicZealous(5, 6, -171);
+     Console.WriteLine(b1);
+     Console.WriteLine(b2);
+     Console.WriteLine(b2.Equals(b1.Opp()));
+     Console.WriteLine(b3);
+     Console.WriteLine(b4);
+     Console.WriteLine(b3.Equals(b4.Opp()));
+     Console.WriteLine(b1 * 5.Pow(4));
+     Console.WriteLine(b1 / 5.Pow(4));
+     Console.WriteLine();
+     
+     Console.WriteLine("Serie c");
+     var c1 = new PadicZealous(3, 3, 101);
+     var c2 = new PadicZealous(3, 6, 101);
+     var c3 = new PadicZealous(3, 4, -101);
+     var c4 = c3.Inv();
+     Console.WriteLine(c1);
+     Console.WriteLine(c1 * 3.Pow(2));
+     Console.WriteLine(c1 / 3.Pow(2));
+     Console.WriteLine(c2);
+     Console.WriteLine(c2 * 3.Pow(2));
+     Console.WriteLine(c2 / 3.Pow(2));
+     Console.WriteLine(c3);
+     Console.WriteLine(c3 * 3.Pow(2));
+     Console.WriteLine(c3 / 3.Pow(2));
+     Console.WriteLine((c3 * c4).Equals(c3.One));
+     Console.WriteLine((c3 * c3 * c3).Equals(c3.Pow(3)));
+     Console.WriteLine((c4 * c4).Equals(c3.Pow(-2)));
+     Console.WriteLine((c4 * c4).Equals(c3.Pow(2).Inv()));
+     Console.WriteLine();
+     
+     Console.WriteLine("Serie d");
+     var d1 = new PadicZealous(3, 7, new Rational(101));
+     var d2 = new PadicZealous(3, 7, new Rational(909));
+     var d3 = new PadicZealous(3, 7, new Rational(101, 9));
+     Console.WriteLine(d1);
+     Console.WriteLine(d1 * 3.Pow(2));
+     Console.WriteLine(d1 / 3.Pow(2));
+     Console.WriteLine(d2);
+     Console.WriteLine(d2 * 3.Pow(3));
+     Console.WriteLine(d2 / 3.Pow(3));
+     Console.WriteLine(d3);
+     Console.WriteLine(d3 * 3);
+     Console.WriteLine(d3 / 3);
+     Console.WriteLine(d2.Equals(9 * d1));
+     Console.WriteLine(d3.Equals(d1 / 9));
+     Console.WriteLine();
+     
+     Console.WriteLine("Serie e");
+     var e1 = new PadicZealous(3, 7, new Rational(909) + new Rational(101, 9));
+     var e2 = new PadicZealous(3, 11, new Rational(909) + new Rational(101, 9));
+     var e3 = new PadicZealous(5, 17, new Rational(313, 3000));
+     Console.WriteLine(e1);
+     Console.WriteLine(d1 / 9 + d2);
+     Console.WriteLine(e1 * 3);
+     Console.WriteLine(e1 * 9);
+     Console.WriteLine(e1 / 3);
+     Console.WriteLine(e1 / 9);
+     Console.WriteLine(e2 * 3.Pow(4));
+     Console.WriteLine(e2 / 3.Pow(4));
+     Console.WriteLine(e3);
+     Console.WriteLine(e3 * 5.Pow(2));
+     Console.WriteLine(e3 / 5.Pow(2));
+     Console.WriteLine(e1.Equals(d1 / 9 + d2));
+     Console.WriteLine(e1.Equals(d2 + d3));
+     Console.WriteLine();
+
+    Console.WriteLine("Serie f");
+    var f1 = new PadicZealous(5, 17, new Rational(313, 75)) * 5.Pow(4);
+    var f2 = new PadicZealous(5, 17, 415) / 5.Pow(2);
+    Console.WriteLine(f1);
+    Console.WriteLine(f2);
+    Console.WriteLine(f1 + f2);
+    Console.WriteLine((f1 + f2) * 125);
+    Console.WriteLine(f1 * 125);
+    Console.WriteLine((f1 + f2) * 3125);
+    Console.WriteLine(f1 * 3125);
+    Console.WriteLine(f1 * 3125 + f2);
+
+    var f3 = f1 * 125 + f2 * 125;
+    var f4 = (f1 + f2) * 125;
+    Console.WriteLine(f3);
+    Console.WriteLine(f4);
+    Console.WriteLine(f3.PNVSM.Equals(f4.PNVSM));
+    Console.WriteLine((f3 - f4));
+    Console.WriteLine((f3 - f4).Norm.IsZero());
     Console.WriteLine();
 
-    Console.WriteLine("Inverse zealous");
-    {
-        var a = QpAdic.Convert(2, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1);
-        var a0 = a + a.Ppow(2);
-        Console.WriteLine($"c = {a}");
-        Console.WriteLine($"a0 = {a0}");
-        for (int i = 0; i < 7; i++)
-        {
-            Console.WriteLine($"Step {i} : {a0}");
-            a0 = 2 * a0 - a * a0.Pow(2);
-        }
-        Console.WriteLine($"Check a^-1 = {a.Inv()} => {a0.Inv().Equals(a)}");
-    }
-    Console.WriteLine();
-}
-
-{
-    Console.WriteLine("Square Root floating padic");
-    {
-        var a = Padic.Convert(2, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1);
-        var a0 = a.Add(a.Ppow(2));
-        Console.WriteLine($"c = {a}");
-        Console.WriteLine($"a0 = {a0}");
-        for (int i = 0; i < 7; i++)
-        {
-            Console.WriteLine($"Step {i} : {a0}");
-            a0 = a0.Add(a.CompleteDivision(a0)).CompleteDivision(a.One.Mul(2));
-        }
-        Console.WriteLine($"Check ai^2 = {a0.Pow(2)} => {a0.Pow(2).Equals(a)}");
-    }
-    Console.WriteLine();
-
-    Console.WriteLine("Square Root zealous");
-    {
-        var a = QpAdic.Convert(2, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1);
-        var a0 = a + a.Ppow(2);
-        Console.WriteLine($"c = {a}");
-        Console.WriteLine($"a0 = {a0}");
-        for (int i = 0; i < 7; i++)
-        {
-            Console.WriteLine($"Step {i} : {a0}");
-            a0 = (a0 + a / a0) / 2;
-        }
-        Console.WriteLine($"Check ai^2 = {a0.Pow(2)} => {a0.Pow(2).Equals(a)}");
-    }
+    Console.WriteLine(f1);
+    Console.WriteLine(f1.Norm);
+    Console.WriteLine(f1.Normalized);
+    Console.WriteLine(f1.Normalized / f1.Norm);
+    Console.WriteLine(f1 * f1.Norm);
+    Console.WriteLine(f1.Normalized.Equals(f1 * f1.Norm));
     Console.WriteLine();
 }
--- Tests/PadicUnitTest.cs
+++ Tests/PadicUnitTest.cs
@@ -12,8 +12,9 @@ public class PadicUnitTest
     {
         Assert.Throws<ArgumentException>(() => Valuation.Infinity.V);
         Assert.Throws<ArgumentException>(() => ZnBInt.KZero(1));
-        Assert.Throws<ArgumentException>(() => new QpAdic(6, 5));
-        Assert.Throws<ArgumentException>(() => new QpAdic(3, -5));
+        Assert.Throws<ArgumentException>(() => new PadicGAP(6, 5));
+        Assert.Throws<ArgumentException>(() => new PadicGAP(3, -5));
+        Assert.Throws<ArgumentException>(() => new PadicZealous(-3, -5));
     }
 
     [Fact]
@@ -27,15 +28,15 @@ public class PadicUnitTest
         var z10 = new ZnBInt(mod1, 12);
         var z11 = new ZnBInt(mod1, 7);
         var z12 = new ZnBInt(mod1, 8);
-        
+
         Assert.Equal(z00.K, z10.K);
         Assert.Equal(-z00.K, -z10.K);
         Assert.Equal((z00 + z01).K, (z10 + z11).K);
         Assert.Equal((z00 - z01).K, (z10 - z11).K);
         Assert.Equal(z00.Pow(3).K, z10.Pow(3).K);
         Assert.Equal((z00 * z01).K, (z10 * z11).K);
-        Assert.Equal((1 / z00).K,(1 / z10).K);
-        Assert.Equal((z00 / z01).K,(z10 / z11).K);
+        Assert.Equal((1 / z00).K, (1 / z10).K);
+        Assert.Equal((z00 / z01).K, (z10 / z11).K);
 
         Assert.Equal(Ring.Gcd(z01, z02).K, Ring.Gcd(z11, z12).K);
         var bez0 = Ring.Bezout(z01, z02);
@@ -46,11 +47,11 @@ public class PadicUnitTest
     [Fact]
     public void Test3PadicIntegers()
     {
-        var a0 = new QpAdic(5, 3);
-        var a1 = new QpAdic(5, 3, 101);
-        var a2 = new QpAdic(5, 3, 75);
-        var a3 = new QpAdic(5, 3, 15);
-        var a4 = a3 / 125; 
+        var a0 = new PadicGAP(5, 3);
+        var a1 = new PadicGAP(5, 3, 101);
+        var a2 = new PadicGAP(5, 3, 75);
+        var a3 = new PadicGAP(5, 3, 15);
+        var a4 = a3 / 125;
 
         Assert.Equal(0, a0.K);
         Assert.Equal(101, a1.K);
@@ -65,30 +66,88 @@ public class PadicUnitTest
 
         Assert.Equal(a1.Zero, (a1 + a1.Opp()));
         Assert.Equal(a1.One, (a1 * a1.Inv()));
-        Assert.Equal(new QpAdic(5, 3, 51), a1 + a2);
-        Assert.Equal(new QpAdic(5, 3, 26), a1 - a2);
-        Assert.Equal(new QpAdic(5, 3, 53, 2), a1 * a2); // val(75) = 2 and 101*3 mod 125 = 303 mod 125 = 53 
-        Assert.Equal(new QpAdic(5, 3, 117, -2), a1 / a2); // val(75) = 2 501=1+125*4=3*167 and 101*167=16867 mod 125=117
-        Assert.Equal(new QpAdic(5, 3, 76), a1.Pow(2)); // 101^2=10201 mod 125 = 76
+        Assert.Equal(new PadicGAP(5, 3, 51), a1 + a2);
+        Assert.Equal(new PadicGAP(5, 3, 26), a1 - a2);
+        Assert.Equal(new PadicGAP(5, 3, 53, 2), a1 * a2); // val(75) = 2 and 101*3 mod 125 = 303 mod 125 = 53 
+        Assert.Equal(new PadicGAP(5, 3, 117, -2),
+            a1 / a2); // val(75) = 2 501=1+125*4=3*167 and 101*167=16867 mod 125=117
+        Assert.Equal(new PadicGAP(5, 3, 76), a1.Pow(2)); // 101^2=10201 mod 125 = 76
     }
 
     [Fact]
     public void Test4PadicRational()
     {
-        var a1 = new QpAdic(5, 7, 171);
-        var a2 = new QpAdic(5, 7, new Rational(171, 9));
-        var a3 = new QpAdic(5, 7, new Rational(21, 9));
-        var a4 = new QpAdic(5, 7, new Rational(15, 11));
-        var a5 = new QpAdic(5, 7, new Rational(21, 9) + new Rational(15, 11));
-        var a6 = new QpAdic(5, 7, new Rational(21, 9) - new Rational(15, 11));
-        var a7 = new QpAdic(5, 7, new Rational(21, 9) * new Rational(15, 11));
-        var a8 = new QpAdic(5, 7, new Rational(21, 9) / new Rational(15, 11));
-        
+        var a1 = new PadicGAP(5, 7, 171);
+        var a2 = new PadicGAP(5, 7, new Rational(171, 9));
+        var a3 = new PadicGAP(5, 7, new Rational(21, 9));
+        var a4 = new PadicGAP(5, 7, new Rational(15, 11));
+        var a5 = new PadicGAP(5, 7, new Rational(21, 9) + new Rational(15, 11));
+        var a6 = new PadicGAP(5, 7, new Rational(21, 9) - new Rational(15, 11));
+        var a7 = new PadicGAP(5, 7, new Rational(21, 9) * new Rational(15, 11));
+        var a8 = new PadicGAP(5, 7, new Rational(21, 9) / new Rational(15, 11));
+
         Assert.Equal(a1 / 9, a2);
         Assert.Equal(a2.Normalized, a2 * a2.Norm);
         Assert.Equal(a5, a3 + a4);
         Assert.Equal(a6, a3 - a4);
         Assert.Equal(a7, a3 * a4);
         Assert.Equal(a8, a3 / a4);
+    }
+
+    [Fact]
+    public void Test5PadicZealousIntegers()
+    {
+        var a0 = new PadicZealous(5, 3);
+        var a1 = new PadicZealous(5, 3, 101);
+        var a2 = new PadicZealous(5, 3, 75);
+        var a3 = new PadicZealous(5, 3, 15);
+        var a4 = a3 / 125;
+
+        Assert.Equal(0, a0.S);
+        Assert.Equal(101, a1.S);
+        Assert.Equal(3, a2.S);
+        Assert.Equal(3, a3.S);
+        Assert.Equal(0, a4.S);
+        Assert.Equal(a0.N, a0.Val);
+        Assert.Equal(0, a1.Val);
+        Assert.Equal(2, a2.Val);
+        Assert.Equal(1, a3.Val);
+        Assert.Equal(a4.N, a4.Val);
+
+        Assert.Equal(a1.Zero, (a1 + a1.Opp()));
+        Assert.Equal(a1.One, (a1 * a1.Inv()));
+        Assert.Equal(new PadicZealous(5, 3, 51), a1 + a2);
+        Assert.Equal(new PadicZealous(5, 3, 26), a1 - a2);
+        Assert.Equal(new PadicZealous(5, 3, 53) * 25, a1 * a2); 
+        Assert.Equal(new PadicZealous(5, 3, 117) / 25, a1 / a2);
+        Assert.Equal(new PadicZealous(5, 3, 76), a1.Pow(2));
+    }
+    
+    [Fact]
+    public void Test6PadicZealousRational()
+    {
+        var a1 = new PadicZealous(5, 7, 171);
+        var a2 = new PadicZealous(5, 7, new Rational(171, 9));
+        var a3 = new PadicZealous(5, 7, new Rational(21, 9));
+        var a4 = new PadicZealous(5, 7, new Rational(15, 11));
+        var a5 = new PadicZealous(5, 7, new Rational(21, 9) + new Rational(15, 11));
+        var a6 = new PadicZealous(5, 7, new Rational(21, 9) - new Rational(15, 11));
+        var a7 = new PadicZealous(5, 7, new Rational(21, 9) * new Rational(15, 11));
+        var a8 = new PadicZealous(5, 7, new Rational(21, 9) / new Rational(15, 11));
+
+        Assert.Equal(a1 / 9, a2);
+        Assert.Equal(a2.Normalized, a2 * a2.Norm);
+        Assert.Equal(a5, a3 + a4);
+        Assert.Equal(a6, a3 - a4);
+        Assert.Equal(a7, a3 * a4);
+        Assert.Equal(a8, a3 / a4);
+
+        var f1 = new PadicZealous(5, 17, new Rational(313, 75)) * 625;
+        var f2 = new PadicZealous(5, 17, 415) / 25;
+        var f3 = f1 * 125 + f2 * 125;
+        var f4 = (f1 + f2) * 125;
+        Assert.Equal(f3, f4);
+        Assert.NotEqual(f3.PNVSM, f4.PNVSM);
+        Assert.True((f3 - f4).Norm.IsZero());
     }
 }
