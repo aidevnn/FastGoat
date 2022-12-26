59010c8cba31e173a8fbbdff858e00b2924487f1
aidevnn <qadassi.ilyas@gmail.com>
2022-12-26 21:46:40 +0300

    Cleaning work table

--- FastGoat/Program.cs
+++ FastGoat/Program.cs
@@ -15,6 +15,7 @@ using FastGoat.UserGroup.Words.ToddCoxeter;
 using static FastGoat.Commons.IntExt;
 using static FastGoat.Commons.EnumerableExt;
 using System.Numerics;
+using System.Runtime.InteropServices;
 using System.Xml;
 using FastGoat.UserGroup.Padic;
 
@@ -25,22 +26,3 @@ using FastGoat.UserGroup.Padic;
 //////////////////////////////////
 
 Console.WriteLine("Hello World");
-
-{
-     Console.WriteLine("Qp Zealous P-Adic Numbers");
-     Console.WriteLine(new PadicZealous(5, 3));
-     Console.WriteLine();
-
-     Console.WriteLine("Serie a");
-     var a1 = new PadicZealous(5, 7, 86);
-     Console.WriteLine(a1);
-     Console.WriteLine(a1.Inv());
-     Console.WriteLine(a1.Inv() * a1);
-     Console.WriteLine(a1 * 5.Pow(2));
-     Console.WriteLine(a1 / 5.Pow(2));
-     Console.WriteLine(a1 * 5.Pow(5)); // one incorrect digit, 86~O(5^2) and 5^5~O(5^5) and the limit is O(5^7)
-     Console.WriteLine(a1 / 5.Pow(5)); // one incorrect digit
-
-     Console.WriteLine(86 * a1.Ppow(5));
-     Console.WriteLine(86 / a1.Ppow(5));
-}
