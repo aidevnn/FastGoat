2600e0121642be9a8e67635017f6f240d78693b1
aidevnn <qadassi.ilyas@gmail.com>
2023-09-24 00:44:10 +0300

    Extension from Twisted Action, other version

--- FastGoat/Structures/GenericGroup/ExtensionGroup.cs
+++ FastGoat/Structures/GenericGroup/ExtensionGroup.cs
@@ -6,15 +6,17 @@ namespace FastGoat.Structures.GenericGroup;
 public readonly struct ExtensionGroup<Tn, Tg> : IGroup<Ep2<Tn, Tg>> where Tg : struct, IElt<Tg> where Tn : struct, IElt<Tn>
 {
     public MapGroups<Ep2<Tg, Tg>, Tn> Map { get; }
+    public Homomorphism<Tg, Automorphism<Tn>> L { get; }
     public ConcreteGroup<Tg> G { get; }
     public ConcreteGroup<Tn> N { get; }
     private HashSet<Ep2<Tn, Tg>> Elements { get; }
 
-    public ExtensionGroup(ConcreteGroup<Tn> n, MapGroups<Ep2<Tg, Tg>, Tn> map, ConcreteGroup<Tg> g)
+    public ExtensionGroup(ConcreteGroup<Tn> n, Homomorphism<Tg, Automorphism<Tn>> l, MapGroups<Ep2<Tg, Tg>, Tn> map, ConcreteGroup<Tg> g)
     {
         G = g;
         N = n;
         Map = map;
+        L = new(G, new Dictionary<Tg, Automorphism<Tn>>(l.HomMap));
         var nName = N.Name.Contains('x') ? $"({N})" : $"{N}";
         var gName = G.Name.Contains('x') ? $"({G})" : $"{G}";
         Name = $"{nName} . {gName}";
@@ -51,21 +53,31 @@ public readonly struct ExtensionGroup<Tn, Tg> : IGroup<Ep2<Tn, Tg>> where Tg : s
 
     public Ep2<Tn, Tg> Invert(Ep2<Tn, Tg> e)
     {
-        var (n, t) = (e.E1, e.E2);
+        // var (n, t) = (e.E1, e.E2);
+        // var ni = N.Invert(n);
+        // var s = G.Invert(t);
+        // var wi = N.Invert(Map[new(s, t)]);
+        // var m = N.Op(wi, ni);
+        // return new(m, s);
+        var (n, g) = (e.E1, e.E2);
         var ni = N.Invert(n);
-        var s = G.Invert(t);
-        var wi = N.Invert(Map[new(s, t)]);
-        var m = N.Op(wi, ni);
-        return new(m, s);
+        var gi = G.Invert(g);
+        var nop = N.Op(Map[new(gi, g)], ni);
+        var m = L[g][nop];
+        return new(m, gi);
     }
 
     public Ep2<Tn, Tg> Op(Ep2<Tn, Tg> e1, Ep2<Tn, Tg> e2)
     {
         // “twisted” multiplication ·ω on N × G by (m, s) ·ω (n, t) := (mnω(s, t), st);
-        var (m, n, s, t) = (e1.E1, e2.E1, e1.E2, e2.E2);
-        var st = G.Op(s, t);
-        var mn = N.Op(m, n);
-        return new(N.Op(mn, Map[new(s, t)]), st);
+        // var (m, n, s, t) = (e1.E1, e2.E1, e1.E2, e2.E2);
+        // var st = G.Op(s, t);
+        // var mn = N.Op(m, n);
+        // return new(N.Op(mn, Map[new(s, t)]), st);
+        var (n1, n2, g1, g2) = (e1.E1, e2.E1, e1.E2, e2.E2);
+        var nop = N.Op(L[g2].Invert()[n1], n2);
+        var g1g2 = G.Op(g1, g2);
+        return new(N.Op(Map[new(g1, g2)], nop), g1g2);
     }
 
     public override int GetHashCode() => Hash;
