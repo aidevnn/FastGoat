ca6846774d867b2c4623fd384ba3bce7b54df0dc
aidevnn <qadassi.ilyas@gmail.com>
2023-10-07 06:12:24 +0300

    Rewrite ExtensionGroup

--- FastGoat/Structures/GenericGroup/ExtensionGroup.cs
+++ FastGoat/Structures/GenericGroup/ExtensionGroup.cs
@@ -1,73 +1,28 @@
-using System.Collections;
 using FastGoat.Structures.CartesianProduct;
 
 namespace FastGoat.Structures.GenericGroup;
 
 public class ExtensionGroup<Tn, Tg> : ConcreteGroup<Ep2<Tn, Tg>> where Tg : struct, IElt<Tg> where Tn : struct, IElt<Tn>
 {
-    public MapElt<Ep2<Tg, Tg>, Tn> Map { get; }
-    public MapElt<Tg, Automorphism<Tn>> L { get; }
-    public ConcreteGroup<Tg> G { get; }
-    public ConcreteGroup<Tn> N { get; }
+    public MapElt<Ep2<Tg, Tg>, Tn> Map => ExtBase.Map;
+    public MapElt<Tg, Automorphism<Tn>> L => ExtBase.L;
+    public ConcreteGroup<Tg> G => ExtBase.G;
+    public ConcreteGroup<Tn> N => ExtBase.N;
+
+    public ExtensionGroupBase<Tn, Tg> ExtBase { get; }
 
     public ExtensionGroup(ConcreteGroup<Tn> n, MapElt<Tg, Automorphism<Tn>> l, MapElt<Ep2<Tg, Tg>, Tn> map, ConcreteGroup<Tg> g)
-    :base("Ext",  Product.Group(n, g), true)
+        : this(new ExtensionGroupBase<Tn, Tg>(n, l, map, g))
     {
-        G = g;
-        N = n;
-        Map = map;
-        L = l.Clone();
-        var nName = N.Name.Contains('x') ? $"({N})" : $"{N}";
-        var gName = G.Name.Contains('x') ? $"({G})" : $"{G}";
+    }
+
+    private ExtensionGroup(ExtensionGroupBase<Tn, Tg> ExtBase) : base("Ext", ExtBase)
+    {
+        this.ExtBase = ExtBase;
+        var nName = ExtBase.N.Name.Contains('x') ? $"({N})" : $"{N}";
+        var gName = ExtBase.G.Name.Contains('x') ? $"({G})" : $"{G}";
         Name = $"{nName} . {gName}";
-
-        List<Ep2<Tn, Tg>> generators = new List<Ep2<Tn, Tg>>();
-        foreach (var e in G.GetGenerators())
-            generators.Add(Product.Elt(N.Neutral(), e));
-
-        foreach (var e in N.GetGenerators())
-            generators.Add(Product.Elt(e, G.Neutral()));
-        
-        var (tmpElements, uniqueGenerators) = Group.UniqueGenerators(this, generators.ToArray());
-        PseudoGenerators = new(uniqueGenerators);
-        Elements = tmpElements;
-        ElementsOrders = Group.ElementsOrders(this, Elements);
-        GroupType = Group.IsCommutative(this, PseudoGenerators)
-            ? GroupType.AbelianGroup
-            : GroupType.NonAbelianGroup;
         Hash = (Name, Elements.Count).GetHashCode();
-    }
-
-    public override Ep2<Tn, Tg> Neutral() => new(N.Neutral(), G.Neutral());
-
-    public override Ep2<Tn, Tg> Invert(Ep2<Tn, Tg> e)
-    {
-        // var (n, t) = (e.E1, e.E2);
-        // var ni = N.Invert(n);
-        // var s = G.Invert(t);
-        // var wi = N.Invert(Map[new(s, t)]);
-        // var m = N.Op(wi, ni);
-        // return new(m, s);
-        var (n, g) = (e.E1, e.E2);
-        var ni = N.Invert(n);
-        var gi = G.Invert(g);
-        var nop = N.Op(Map[new(gi, g)], ni);
-        var m = L[g][nop];
-        return new(m, gi);
-    }
-
-    public override Ep2<Tn, Tg> Op(Ep2<Tn, Tg> e1, Ep2<Tn, Tg> e2)
-    {
-        // “twisted” multiplication ·ω on N × G by (m, s) ·ω (n, t) := (mnω(s, t), st);
-        // var (m, n, s, t) = (e1.E1, e2.E1, e1.E2, e2.E2);
-        // var st = G.Op(s, t);
-        // var mn = N.Op(m, n);
-        // return new(N.Op(mn, Map[new(s, t)]), st);
-        var (n1, n2, g1, g2) = (e1.E1, e2.E1, e1.E2, e2.E2);
-        var nop = N.Op(L[g2].Invert()[n1], n2);
-        var fi = N.Invert(Map[new(g1, g2)]);
-        var g1g2 = G.Op(g1, g2);
-        return new(N.Op(fi, nop), g1g2);
     }
 
     public override int GetHashCode() => Hash;
---
+++ FastGoat/Structures/GenericGroup/ExtensionGroupBase.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using FastGoat.Structures.CartesianProduct;
+
+namespace FastGoat.Structures.GenericGroup;
+
+public struct ExtensionGroupBase<Tn, Tg> : IGroup<Ep2<Tn, Tg>> where Tg : struct, IElt<Tg> where Tn : struct, IElt<Tn>
+{
+    public MapElt<Ep2<Tg, Tg>, Tn> Map { get; }
+    public MapElt<Tg, Automorphism<Tn>> L { get; }
+    public ConcreteGroup<Tg> G { get; }
+    public ConcreteGroup<Tn> N { get; }
+
+    public ExtensionGroupBase(ConcreteGroup<Tn> n, MapElt<Tg, Automorphism<Tn>> l, MapElt<Ep2<Tg, Tg>, Tn> map, ConcreteGroup<Tg> g)
+    {
+        G = g;
+        N = n;
+        Map = map;
+        L = l.Clone();
+        var nName = N.Name.Contains('x') ? $"({N})" : $"{N}";
+        var gName = G.Name.Contains('x') ? $"({G})" : $"{G}";
+        Name = $"{nName} . {gName}";
+
+        Elements = Product.Generate(N, G).ToHashSet();
+
+        PseudoGenerators = new List<Ep2<Tn, Tg>>();
+        foreach (var e in G.GetGenerators())
+            PseudoGenerators.Add(Product.Elt(N.Neutral(), e));
+
+        foreach (var e in N.GetGenerators())
+            PseudoGenerators.Add(Product.Elt(e, G.Neutral()));
+
+        Hash = (Name, Elements.Count).GetHashCode();
+        // IsGroup = Group.IsGroup(this, Elements);
+        IsGroup = true;
+    }
+    
+    public bool IsGroup { get; }
+
+    public List<Ep2<Tn, Tg>> PseudoGenerators { get; }
+    public HashSet<Ep2<Tn, Tg>> Elements { get; }
+
+    public IEnumerator<Ep2<Tn, Tg>> GetEnumerator() => GetElements().GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public bool Equals(IGroup<Ep2<Tn, Tg>>? other) => other?.Hash == Hash;
+
+    public int Hash { get; }
+    public string Name { get; }
+
+    public Ep2<Tn, Tg> this[params ValueType[] us]
+    {
+        get
+        {
+            var (us0, us1) = (us[0], us[1]);
+            return new(N[us0], G[us1]);
+        }
+    }
+
+    public IEnumerable<Ep2<Tn, Tg>> GetElements() => Elements;
+
+    public IEnumerable<Ep2<Tn, Tg>> GetGenerators() => PseudoGenerators;
+
+    public Ep2<Tn, Tg> Neutral() => new(N.Neutral(), G.Neutral());
+
+    public Ep2<Tn, Tg> Invert(Ep2<Tn, Tg> e)
+    {
+        var (n, g) = (e.E1, e.E2);
+        var ni = N.Invert(n);
+        var gi = G.Invert(g);
+        var nop = N.Op(Map[new(gi, g)], ni);
+        var m = L[g][nop];
+        return new(m, gi);
+    }
+
+    public Ep2<Tn, Tg> Op(Ep2<Tn, Tg> e1, Ep2<Tn, Tg> e2)
+    {
+        var (n1, n2, g1, g2) = (e1.E1, e2.E1, e1.E2, e2.E2);
+        var nop = N.Op(L[g2].Invert()[n1], n2);
+        var fi = N.Invert(Map[new(g1, g2)]);
+        var g1g2 = G.Op(g1, g2);
+        return new(N.Op(fi, nop), g1g2);
+    }
+
+    public override int GetHashCode() => Hash;
+    public override string ToString() => Name;
+}
