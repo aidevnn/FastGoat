803f9cbd8222f785ffd5741702088a6af3832f58
aidevnn <qadassi.ilyas@gmail.com>
2023-11-02 09:01:03 +0300

    Record for system solutions

---
+++ FastGoat/UserGroup/GModuleN/SysSolution.cs
@@ -0,0 +1,7 @@
+using FastGoat.Structures;
+
+namespace FastGoat.UserGroup.GModuleN;
+
+public record SysSolution<Tn,Tg>(int total, List<SysReduction> sreds, CrMap<Tn,Tg> allMaps) 
+    where Tg : struct, IElt<Tg>
+    where Tn : struct, IElt<Tn>;
