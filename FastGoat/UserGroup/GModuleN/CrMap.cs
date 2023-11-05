a6d07e4658a842363498d6e696f2b1c0e6c2077f
aidevnn <qadassi.ilyas@gmail.com>
2023-11-05 07:38:38 +0300

    CrMap pseudo generators, convert unknowns to explicit MapGroup

--- FastGoat/UserGroup/GModuleN/CrMap.cs
+++ FastGoat/UserGroup/GModuleN/CrMap.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using FastGoat.Commons;
 using FastGoat.Structures;
 using FastGoat.Structures.CartesianProduct;
 using FastGoat.Structures.GenericGroup;
@@ -7,8 +8,8 @@ using FastGoat.UserGroup.Integers;
 
 namespace FastGoat.UserGroup.GModuleN;
 
-public readonly struct CrMap<Tn, Tg> : IEnumerable<KeyValuePair<Ep<Tg>, ZNElt<Tn,Tg>>> , IEquatable<CrMap<Tn,Tg>>
-    where Tg : struct, IElt<Tg> 
+public readonly struct CrMap<Tn, Tg> : IEnumerable<KeyValuePair<Ep<Tg>, ZNElt<Tn, Tg>>>, IEquatable<CrMap<Tn, Tg>>
+    where Tg : struct, IElt<Tg>
     where Tn : struct, IElt<Tn>
 {
     private Dictionary<Ep<Tg>, ZNElt<Tn, Tg>> Map { get; }
@@ -17,13 +18,14 @@ public readonly struct CrMap<Tn, Tg> : IEnumerable<KeyValuePair<Ep<Tg>, ZNElt<Tn
     {
         Map = new();
     }
-    
+
     public CrMap(Dictionary<Ep<Tg>, ZNElt<Tn, Tg>> map)
     {
         Map = map;
     }
-    
+
     public int R => Map.Count == 0 ? 0 : Map.First().Key.Ei.Length;
+    public Gp<Tg> Gr => Product.Gp(G, R);
     public AbelianDirectSum<Tn> Nab => Map.First().Value.Nab;
     public ConcreteGroup<Tn> N => Map.First().Value.N;
     public ConcreteGroup<Tg> G => Map.First().Value.G;
@@ -60,25 +62,102 @@ public readonly struct CrMap<Tn, Tg> : IEnumerable<KeyValuePair<Ep<Tg>, ZNElt<Tn
     public CrMap<Tn, Tg> Add(CrMap<Tn, Tg> cr) => new(Map.ToDictionary(e => e.Key, e => (e.Value + cr[e.Key]).Simplify()));
     public CrMap<Tn, Tg> Sub(CrMap<Tn, Tg> cr) => new(Map.ToDictionary(e => e.Key, e => (e.Value - cr[e.Key]).Simplify()));
     public CrMap<Tn, Tg> Mul(Polynomial<ZnInt, Xi> c) => new(Map.ToDictionary(e => e.Key, e => (e.Value * c).Simplify()));
+    public CrMap<Tn, Tg> Mul(int c) => new(Map.ToDictionary(e => e.Key, e => (c * e.Value).Simplify()));
 
-    public (int mod, int ord, CrMap<Tn, Tg> map)[] Generators()
+    public CrMap<Tn, Tg> ConsTerm()
+    {
+        var xis = Map.SelectMany(e => e.Value.Coefs.Values.SelectMany(p => p.ExtractAllIndeterminates)).ToHashSet();
+        var zero = PZero;
+        var subs = xis.Select(xi => (zero, xi)).ToArray();
+        return Substitute(subs);
+    }
+
+    public CrMap<Tn, Tg>[] Cycle()
+    {
+        if (!Equals(ConsTerm()))
+        {
+            ZNSolver.DisplayCrMap("Error Cycle", this, ConsTerm());
+            throw new("Only constant map");
+        }
+
+        var cycle = new List<CrMap<Tn, Tg>>() { Zero };
+        while (true)
+        {
+            var m0 = Add(cycle.Last());
+            if (m0.IsZero())
+                break;
+            cycle.Add(m0);
+        }
+
+        return cycle.ToArray();
+    }
+
+    public (int ord, CrMap<Tn, Tg> map)[] Generators()
     {
         var zero = PZero;
         var xis = Map.SelectMany(e => e.Value.Coefs.Values.SelectMany(p => p.ExtractAllIndeterminates)).ToHashSet();
+        if (xis.Count == 0)
+            return new[] { (1, Zero) };
+
         var allSubs = xis.OrderBy(e => e.xi).Select(xi => (xi, xis.Except(new[] { xi }).Select(yi => (zero, yi)).ToArray())).ToArray();
         var cr = this;
-        var gens = allSubs.Select(subs => (subs.xi, map:cr.Substitute(subs.Item2))).ToArray();
-        var nab = Nab;
-        var orders = nab.ElemOrders;
-        return gens.Select(g =>
-                (g.xi, g.map, g.map.SelectMany(e => e.Value.Coefs
-                    .Select(c => (mod: nab.DecompElementaryMap[c.Key],
-                        ord: c.Value.Coefs.Values.Max(z => orders[nab.DecompElementaryMap[c.Key]][z.K]))))))
-            .Select(e => (e.Item3.MaxBy(d => d.ord), map: e.map.Substitute(zero.One, e.xi)))
-            .Select(e => (e.Item1.mod, e.Item1.ord, e.map))
-            .OrderByDescending(e => e.map.Sum(c => (c.Value.IsZero() ? 10 : 1) + c.Value.Coefs.Count(p => p.Value.IsZero())))
-            .ThenByDescending(e => e.ord)
-            .ToArray();
+        return allSubs.Select(e => cr.Substitute(e.Item2).Substitute(zero.One, e.xi)).Select(e => (e.Cycle().Length, e))
+            .OrderBy(e => e.Length).ToArray();
+    }
+
+    public CrMap<Tn, Tg> Act(Tg g)
+    {
+        var gr = Gr;
+        var map = Map.ToDictionary(e => gr.Act(g, e.Key), e => e.Value.Act(g).Simplify());
+        return new(map);
+    }
+
+    public ConcreteGroup<MapElt<Ep<Tg>, Tn>> ToGroupMapElt(string name = "")
+    {
+        var gr = Product.GpGenerate(G, R);
+        var N0 = N;
+        var gens = Generators().Select(e => e.map.Map.ToDictionary(e0 => e0.Key, e0 => e0.Value.Expand))
+            .Select(m => new MapElt<Ep<Tg>, Tn>(gr, N0, m)).ToArray();
+        var mapGr = new MapGroupBase<Ep<Tg>, Tn>(gr, N0);
+        var g = Group.Generate(mapGr, gens);
+        if (!string.IsNullOrEmpty(name))
+            g.SetName(name);
+
+        return g;
+    }
+
+    public CrMap<Tn, Tg> ToHomogenous()
+    {
+        Ep<Tg> Chg(Ep<Tg> e, ConcreteGroup<Tg> g)
+        {
+            var r = e.Ei.Length + 1;
+            var e0 = r.Range().Select(k => g.OpSeq(e.Ei.Take(k))).ToArray();
+            return new(e0);
+        }
+
+        var G0 = G;
+        var map0 = Map.ToDictionary(e => Chg(e.Key, G0), e => e.Value);
+        var Gr1 = Product.Gp(G, R + 1);
+        var map1 = G.SelectMany(g => map0.ToDictionary(e => Gr1.Act(g, e.Key), e => e.Value.Act(g).Simplify()))
+            .ToDictionary(e => e.Key, e => e.Value);
+
+        return new(map1);
+    }
+
+    public CrMap<Tn, Tg> ToInhomogenous()
+    {
+        Ep<Tg> Chg(Ep<Tg> e, ConcreteGroup<Tg> g)
+        {
+            var r = e.Ei.Length + 1;
+            var e0 = r.Range().Select(k => g.OpSeq(e.Ei.Take(k))).ToArray();
+            return new(e0);
+        }
+
+        var Gr1 = Product.Gp(G, R - 1);
+        var G0 = G;
+        var map = Map;
+        var map0 = Gr1.ToDictionary(e => e, e => map[Chg(e, G0)]);
+        return new(map0);
     }
 
     public Xi[] ExtractAllIndeterminates => Map.Values.SelectMany(c => c.ExtractAllIndeterminates).Distinct().Order().ToArray();
