17d005d91a90450c23fc37be10b5792c74dc5ce0
aidevnn <qadassi.ilyas@gmail.com>
2023-11-14 12:10:41 +0300

    H2 Test parameters

--- FastGoat/UserGroup/GModuleN/TestTwoCohomology.cs
+++ FastGoat/UserGroup/GModuleN/TestTwoCohomology.cs
@@ -48,8 +48,8 @@ public static class TestTwoCohomology
             var L = op.ToMapElt(autN);
             var lbl = $"Lbl{i}/{ops.Count}";
             var (cosets, (nb2Cobs, _, maps2Cobs), (nb2Cocs, _, maps2Cocs)) = ZNSolver.Reduce2Cohomologies(N, G, L, lbl);
-            if (nb2Cocs > 1000)
-                return;
+            if (nb2Cocs > 1100)
+                continue;
 
             var cosetsMapElts = cosets.Select(e => e.ToMapElt).ToList();
             var g2Cobs = maps2Cobs.ToGroupMapElt("B2");
@@ -74,7 +74,7 @@ public static class TestTwoCohomology
         }
     }
 
-    private static List<ConcreteGroup<Ep<ZnInt>>> AllAbelianGroupsOrder(int k)
+    public static List<ConcreteGroup<Ep<ZnInt>>> AllAbelianGroupsOrder(int k)
     {
         var dec = IntExt.PrimesDec(k);
         return dec.Select(e => IntExt.Partitions32[e.Value].Select(l => l.Select(i => e.Key.Pow(i)).ToArray())).MultiLoop()
@@ -118,8 +118,16 @@ public static class TestTwoCohomology
         var (ord4, ord8) = (AllAbelianGroupsOrder(4), AllAbelianGroupsOrder(8));
         var ord2_4 = ord4.Prepend(FG.Abelian(2)).ToList();
         var ord2_4_8 = ord2_4.Concat(ord8).ToList();
-        var allProducts = ord2_4_8.Grid2D(ord2_4).ToList();
-        allProducts.ForEach(e => SolveTwoCohom(e.t1, e.t2));
+        
+        {
+            var allProducts = ord2_4.Grid2D(ord2_4_8).ToList();
+            allProducts.ForEach(e => SolveTwoCohom(e.t1, e.t2));
+        }
+        
+        {
+            var allProducts = ord2_4_8.Grid2D(ord2_4).ToList();
+            allProducts.ForEach(e => SolveTwoCohom(e.t1, e.t2));
+        }
     }
 
     public static void Test2Coset()
@@ -141,7 +149,7 @@ public static class TestTwoCohomology
     public static void TestExplicit()
     {
         var o = new[] { 2, 3, 4, 6, 8, 9, 12, 16 };
-        var allProducts = o.Take(3).Grid2D(o).Where(e => e.t1 * e.t2 < 30)
+        var allProducts = o.Take(3).Grid2D(o).Where(e => e.t1 * e.t2 < 33)
             .SelectMany(e => AllAbelianGroupsOrder(e.t2).Grid2D(AllAbelianGroupsOrder(e.t1)))
             .ToList();
 
