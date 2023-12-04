69ed4a0caf58e85265084d20d3490700065304c9
aidevnn <qadassi.ilyas@gmail.com>
2023-12-05 02:03:59 +0300

    Record SubGroupsInfos

--- FastGoat/Structures/GenericGroup/AllSubgroups.cs
+++ FastGoat/Structures/GenericGroup/AllSubgroups.cs
@@ -2,6 +2,11 @@ using System.Collections;
 
 namespace FastGoat.Structures.GenericGroup;
 
+public record SubGroupsInfos(int AllSubGr, int AllConjsCl, int AllNorms)
+{
+    public static explicit operator (int, int, int)(SubGroupsInfos infos) => (infos.AllSubGr, infos.AllConjsCl, infos.AllNorms);
+}
+
 public readonly struct AllSubgroups<T> : IEnumerable<SubgroupConjugates<T>> where T : struct, IElt<T>
 {
     public SubgroupConjugates<T>[] AllSubgroupConjugates { get; }
@@ -10,18 +15,24 @@ public readonly struct AllSubgroups<T> : IEnumerable<SubgroupConjugates<T>> wher
     public AllSubgroups(ConcreteGroup<T> g)
     {
         var allSubs = Group.AllSubGroups(g);
+        Infos = new(allSubs.Values.Sum(s => s.Count), allSubs.Count, allSubs.Count(s => s.Value.Count == 1));
         AllSubgroupConjugates = allSubs.Values.Select(l => new SubgroupConjugates<T>(g, l)).Order().ToArray();
     }
 
     public AllSubgroups(Dictionary<ConcreteGroup<T>, List<ConcreteGroup<T>>> allSubs)
     {
         var g = allSubs.MaxBy(sg => sg.Key.Count()).Key;
+        Infos = new(allSubs.Values.Sum(s => s.Count), allSubs.Count, allSubs.Count(s => s.Value.Count == 1));
         AllSubgroupConjugates = allSubs.Values.Select(l => new SubgroupConjugates<T>(g, l)).Order().ToArray();
     }
 
     private AllSubgroups(HashSet<SubgroupConjugates<T>> all)
     {
         AllSubgroupConjugates = all.Order().ToArray();
+        var conjs = AllSubgroupConjugates.Length;
+        var subs = AllSubgroupConjugates.Sum(sc => sc.Size);
+        var norms = AllSubgroupConjugates.Count(sc => sc.IsNormal);
+        Infos = new(subs, conjs, norms);
     }
 
     public AllSubgroups<T> Restriction(ConcreteGroup<T> g) => new(AllSubgroupConjugates.Select(sc => sc.Restriction(g)).ToHashSet());
@@ -32,6 +43,8 @@ public readonly struct AllSubgroups<T> : IEnumerable<SubgroupConjugates<T>> wher
     {
         return GetEnumerator();
     }
+
+    public SubGroupsInfos Infos { get; }
 
     public string Name => $"SubGroups of {Parent}";
 
