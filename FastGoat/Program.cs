4b1d1fef1d4deb7a76e7010bb82fc75cbe74e689
aidevnn <qadassi.ilyas@gmail.com>
2023-12-25 03:06:05 +0300

    Verify the correctness of the new implementation for abelian group decomposition.

--- FastGoat/Program.cs
+++ FastGoat/Program.cs
@@ -30,3 +30,114 @@ using FastGoat.UserGroup.Padic;
 
 Console.WriteLine("Hello World");
 
+void fancyLog()
+{
+    for (int i = 2; i < 40; i++)
+    {
+        var cn = new Cn(i);
+        foreach (var e in cn.Except([cn.Neutral()]))
+            Console.WriteLine("{0} {1} {2}", cn, e, Group.CycleExceptNeutral(cn, e).Glue(" "));
+        Console.WriteLine();
+    }
+}
+
+void Ord16()
+{
+    GlobalStopWatch.Restart();
+    FG.AllExtensions(
+            (FG.Abelian(8), FG.Abelian(2)),
+            (FG.Abelian(2, 4), FG.Abelian(2)),
+            (FG.Abelian(2, 2, 2), FG.Abelian(2)))
+        .Take(14)
+        .NamingExts()
+        .DisplayExts();
+
+    GlobalStopWatch.Show();
+    Console.Beep();
+}
+
+void Ord20_40()
+{
+    GlobalStopWatch.Restart();
+    var all20 = FG.AllExtensions(
+            (FG.Abelian(5), FG.Abelian(4)),
+            (FG.Abelian(2, 5), FG.Abelian(2)))
+        .Take(5)
+        .NamingExts()
+        .ToArray();
+
+    var all40 = FG.AllExtensions(
+            (FG.Abelian(2, 5), FG.Abelian(4)),
+            (FG.Abelian(2, 5), FG.Abelian(2, 2)))
+        .Take(14)
+        .NamingExts()
+        .ToArray();
+
+    all20.DisplayExts();
+    all40.DisplayExts();
+
+    GlobalStopWatch.Show();
+    Console.Beep();
+}
+
+void Ord32()
+{
+    GlobalStopWatch.Restart();
+    var nbOpsMax = 10000;
+    FG.AllExtensions(
+            (nbOpsMax, FG.Abelian(16), FG.Abelian(2)),
+            (nbOpsMax, FG.Abelian(2, 8), FG.Abelian(2)),
+            (nbOpsMax, FG.Abelian(4, 4), FG.Abelian(2)),
+            (nbOpsMax, FG.Abelian(2, 2, 4), FG.Abelian(2)),
+            (nbOpsMax, FG.Abelian(2, 4), FG.Abelian(4)),
+            (16, FG.Abelian(2, 4), FG.Abelian(2, 2)),
+            (1, FG.Abelian(2, 2, 2), FG.Abelian(2, 2)))
+        .Take(51)
+        .NamingExts()
+        .DisplayExts();
+
+    GlobalStopWatch.Show();
+    Console.Beep();
+    // 
+}
+
+void Ord80()
+{
+    GlobalStopWatch.Restart();
+    FG.AllExtensions(
+            (FG.Abelian(2, 5), FG.Abelian(8)),
+            (FG.Abelian(2, 2, 5), FG.Abelian(4)),
+            (FG.Abelian(4, 5), FG.Abelian(4)),
+            (FG.Abelian(4, 5), FG.Abelian(2, 2)),
+            (FG.Abelian(2, 2, 5), FG.Abelian(2, 2)),
+            (FG.Abelian(2, 2, 2, 2), FG.Abelian(5)))
+        .Take(52)
+        .Select(e => e.allSubs.ToTable())
+        .Naming()
+        .DisplayNames();
+
+    GlobalStopWatch.Show("End");
+    Console.Beep();
+}
+
+void Ord81()
+{
+    GlobalStopWatch.Restart();
+    FG.AllExtensions(FG.AllAbelianGroupsOfOrder(27).Select(e => (e, FG.Abelian(3))).ToArray())
+        .Take(15)
+        .Select(e => e.allSubs.ToTable())
+        .Naming()
+        .DisplayNames();
+
+    GlobalStopWatch.Show("End");
+    Console.Beep();
+}
+
+{
+    // fancyLog();
+    // Ord16();
+    // Ord20_40();
+    // Ord32();
+    // Ord80();
+    Ord81();
+}
