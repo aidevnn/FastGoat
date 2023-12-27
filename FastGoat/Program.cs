f986b3b137580d49388e9d5f4f6b3e3eb1c95cb6
aidevnn <qadassi.ilyas@gmail.com>
2023-12-27 13:53:33 +0300

    260 groups of order 64

--- FastGoat/Program.cs
+++ FastGoat/Program.cs
@@ -217,8 +217,67 @@ void ProductOrd32()
     // fancyLog();
     // Ord16();
     // Ord20_40();
-    Ord32();
+    // Ord32();
     // Ord80();
     // Ord81();
-    TestOrd64();
+}
+
+
+{
+    GlobalStopWatch.Restart();
+    
+    var all4 = FG.AllAbelianGroupsOfOrder(4);
+    var ord8 = new [] { FG.Abelian(8).ToCGTable(), FG.Abelian(4, 2).ToCGTable(), FG.DihedralSdp(4).ToCGTable(), FG.Quaternion(8).ToCGTable() };
+    var ord16 = FG.AllAbelianGroupsOfOrder(16).Where(e => Group.AbelianGroupType(e).Count(c => c == 2) < 4).ToArray();
+    var ord32 = FG.AllAbelianGroupsOfOrder(32).Where(e => Group.AbelianGroupType(e).Count(c => c == 2) < 3).ToArray();
+    var rg = ord8.Length.Range();
+    
+    var list0 = FG.AllAbelianGroupsOfOrder(64).Select(e => e.ToCGTable().AllSubgroups());
+    var list1 = rg.Grid2D(rg).Where(e => e.t1 >= e.t2).Select(e => (t1: ord8[e.t1], t2: ord8[e.t2]))
+        .SelectMany(e => AllProducts(e.t1, e.t2));
+    var list2 = ord16.Grid2D(all4).SelectMany(e => AllProducts(e.t1, e.t2));
+    var list3 = ord32.SelectMany(e => AllProducts(e, new Cn(2)));
+    
+    var nbOpsMax = 10000;
+    var s32 = new Sn(32);
+    var g1 = s32[(1, 2, 3, 4), (5, 6, 7, 8), (9, 10, 11, 12), (13, 14, 15, 16), (17, 18, 19, 20), (21, 22, 23, 24),
+        (25, 26, 27, 28), (29, 30, 31, 32)];
+    var g2 = s32[(1, 17, 15, 6), (2, 18, 16, 7), (3, 19, 13, 8), (4, 20, 14, 5), (9, 29, 24, 28), (10, 30, 21, 25),
+        (11, 31, 22, 26), (12, 32, 23, 27)];
+    var g3 = s32[(1, 26, 15, 31), (2, 32, 16, 27), (3, 28, 13, 29), (4, 30, 14, 25), (5, 12, 20, 23),
+        (6, 24, 17, 9), (7, 10, 18, 21), (8, 22, 19, 11)];
+
+    var sm3232 = Group.Generate("[(C4 x C4) . C2]pg", s32, g1, g2, g3).ToCGTable();
+
+    var c2 = FG.Abelian(2);
+    var sdps1 = AllProducts(FG.Abelian(4), FG.Abelian(8)).FilterIsomorphic().Naming().Select(e => e.subsg.Parent).ToArray();
+    var sdp3 = Product.Generate(c2, FG.ModularMaxSdp(4)).ToCGTable();
+    var sdp4 = Group.AllSemiDirectProd(FG.Abelian(2, 4), FG.Abelian(4)).Select(e => e.ToCGTable()).Naming().ToArray()[2];
+
+    var e245 = (nbOpsMax, 0, sm3232, c2); // Group64 Id 172,245
+    var tuplesC2 =
+        sdps1.Concat([sdp3, sdp4]).Select(e => (nbOpsMax, 0, e, c2)).ToArray(); // Group64 Id 11,13,14,22,79,81,82,160,172,180
+    var tuplesC4 = new[]
+    {
+        (nbOpsMax, 0, FG.Abelian(2, 8).ToCGTable(), FG.Abelian(4)),
+        (nbOpsMax, 0, FG.Abelian(4, 4).ToCGTable(), FG.Abelian(4))
+    }; // Group64 Id 19,22,37,45
+    var tuplesC2C2 = new[]
+    {
+        (nbOpsMax, 0, FG.Abelian(2, 8).ToCGTable(), FG.Abelian(2, 2)),
+        (nbOpsMax, 0, FG.Abelian(4, 4).ToCGTable(), FG.Abelian(2, 2))
+    }; // Group64 Id 49,45,180,160,168,43,172
+    
+    var list4 = FG.AllExtensions([..tuplesC2, ..tuplesC4, e245, ..tuplesC2C2]).Select(e => e.allSubs.ToTable());
+
+    GlobalStopWatch.Restart();
+    list0.AppendIsomorphic(list1, list2, list3, list4)
+        .Take(267)
+        .DisplayBox();
+
+    GlobalStopWatch.Show();
+    Console.Beep();
+    // Total Groups:260
+    // #  Time:1h7m
+    // 
 }
