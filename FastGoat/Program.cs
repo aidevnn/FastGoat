e5e1e6d61d82b5fa04cad97464635c201e5a4957
aidevnn <qadassi.ilyas@gmail.com>
2024-01-05 19:13:19 +0300

    Cleaning work table

--- FastGoat/Program.cs
+++ FastGoat/Program.cs
@@ -29,30 +29,3 @@ using FastGoat.UserGroup.Padic;
 //////////////////////////////////
 
 Console.WriteLine("Hello World");
-
-{
-    GlobalStopWatch.Restart();
-    var ord24 = FG.AllExtensions(
-            (FG.Abelian(12).ToCGTable(), FG.Abelian(2).ToCGTable()),
-            (FG.Abelian(2, 6).ToCGTable(), FG.Abelian(2).ToCGTable()),
-            (FG.Alternate(4).ToCGTable(), FG.Abelian(2).ToCGTable()),
-            (FG.Quaternion(8).ToCGTable(), FG.Abelian(3).ToCGTable())
-        )
-        .Select(e => e.allSubs.ToTable())
-        .FilterIsomorphic()
-        .Naming()
-        .ToArray();
-
-    var listByC2 = ord24.SelectMany(e => FG.AllExtensions((e.subsg.Parent, FG.Abelian(2)))).Select(e => e.allSubs.ToTable());
-    var listByC3 = FG.AllAbelianGroupsOfOrder(16).SelectMany(e => Group.AllSDPFilterLazy(e, FG.Abelian(3)));
-    listByC2.AppendIsomorphic(listByC3)
-        .Take(52)
-        .Naming()
-        .DisplayNames();
-
-    GlobalStopWatch.Show();
-    Console.Beep();
-    // Total Groups:52
-    // #  Time:1m16s
-    // 
-}
