5ad7bfa1aff10533a7ed03e93ee7d0d456f3419d
aidevnn <qadassi.ilyas@gmail.com>
2024-03-19 03:25:09 +0300

    Insertion and Iteration Based Permutation Generation

--- FastGoat/Commons/IntExt.cs
+++ FastGoat/Commons/IntExt.cs
@@ -241,7 +241,7 @@ public static class IntExt
     private static Dictionary<int, int[][]> AllPermutations { get; }
 
     /// <summary>
-    /// Steinhaus–Johnson–Trotter algorithm
+    /// Insertion and Iteration Based Permutation Generation
     /// Generates all possible permutations of a set of numbers. 
     /// </summary>
     /// <param name="n">The number of elements in the set.</param>
