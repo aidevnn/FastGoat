d2850ce3baf7f79b372b9757bfa547389e2387d7
aidevnn <qadassi.ilyas@gmail.com>
2024-04-03 06:28:00 +0300

    Primitive permutation group

--- FastGoat/Program.cs
+++ FastGoat/Program.cs
@@ -32,3 +32,47 @@ using GroupRegX = System.Text.RegularExpressions.Group;
 //////////////////////////////////
 
 Console.WriteLine("Hello World");
+
+Perm.Style = DisplayPerm.CyclesComma;
+
+Ep<ZnInt> Image(Perm g, Ep<ZnInt> x) => new(x.Ei.Select(e => new ZnInt(g.Sn.N, g.Table[e.K])).ToArray());
+HashSet<Ep<ZnInt>> Images(Perm g, Ep<ZnInt>[] Y) => Y.Select(y => Image(g, y)).ToHashSet();
+
+void PrimitivesTransitivesOfSn(int n)
+{
+    var sn = new Symm(n);
+    Console.WriteLine($"Transitives groups of order {n}");
+    var Xn = FG.Abelian(n).ToArray();
+    var allSubgSn = sn.AllSubgroups();
+    allSubgSn.Naming();
+    var Ys = Xn.AllCombinationsFromM(2).Where(y => y.Length != n).ToArray();
+    var dic = new[] { "Primitive", "Imprimitive" }.Grid2D(["Transitve", "NonTransitive"])
+        .ToDictionary(e => e, _ => new List<SubgroupConjugates<Perm>>());
+    foreach (var g1 in allSubgSn.AllRepresentatives)
+    {
+        var allOrbits = Group.AllOrbits(g1, Xn, Image);
+        var blocks = Ys.Where(Y => g1.All(g =>
+        {
+            var Yg = Images(g, Y);
+            return Yg.SetEquals(Y) || !Yg.Overlaps(Y);
+        })).ToArray();
+        var isPrimitve = blocks.Length == 0 ? "Primitive" : "Imprimitive";
+        var isTransitive = allOrbits.Count == 1 ? "Transitve" : "NonTransitive";
+        var cj = allSubgSn.AllSubgroupConjugates.First(cj => cj.Contains(g1));
+        dic[(isPrimitve, isTransitive)].Add(cj);
+    }
+
+    dic.Select(e => ($"{e.Key} => {e.Value.Count}/{e.Value.Sum(cj => cj.Size)}")).Println("Count");
+    dic[("Primitive", "Transitve")].Println(e => e.FullName, "Primitive, Transitve");
+    Console.WriteLine();
+}
+
+{
+    PrimitivesTransitivesOfSn(3);
+    PrimitivesTransitivesOfSn(4);
+    PrimitivesTransitivesOfSn(5);
+    PrimitivesTransitivesOfSn(6);
+    PrimitivesTransitivesOfSn(7);
+    
+    Console.Beep();
+}
