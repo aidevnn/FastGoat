8255f52e9848d267a5182f7e3f271fb589e22f75
aidevnn <qadassi.ilyas@gmail.com>
2024-05-19 04:11:24 +0300

    Crafting. Character table of Metacyclics group in Matrix form.

--- FastGoat/Program.cs
+++ FastGoat/Program.cs
@@ -31,3 +31,50 @@ using GroupRegX = System.Text.RegularExpressions;
 //////////////////////////////////
 
 Console.WriteLine("Hello World");
+
+(int m, int n, int r)[] MetaCyclicSdp(int order)
+{
+    return IntExt.Dividors(order).Where(d => d > 1)
+        .SelectMany(m => FG.MetaCyclicSdpGetR(m, order / m).Select(r => (m, n: order / m, r)))
+        .ToArray();
+}
+
+void AllGensOfMtCycSdpUpToOrder(int maxOrd, int maxDim = 6)
+{
+    GlobalStopWatch.Restart();
+    var missing = new List<(int, int, int)>();
+    var allMtCycSdp = (maxOrd - 5).Range(6).SelectMany(ord => MetaCyclicSdp(ord)).ToArray();
+
+    foreach (var e in allMtCycSdp)
+    {
+        var mtGL = FG.MetaCyclicSdpMat(e.m, e.n, e.r, maxDim);
+        if (mtGL.Count() != 1)
+        {
+            DisplayGroup.HeadOrders(mtGL);
+            var ct = FG.CharacterTable(mtGL);
+            foreach (var (mat, k) in mtGL.GetGenerators().Select((mat,k)=>(mat, k+1)))
+            {
+                Console.WriteLine($"gen{k} of order {ct.Classes.GetClassName(mat)}");
+                Console.WriteLine(mat);
+            }
+
+            ct.DisplayCells(tableOnly: true);
+            var dimCt = ct.AllCharacters.Max(chi => chi[mtGL.Neutral()])!.Value.E[0].Num;
+            if (dimCt < mtGL.Neutral().GL.N)
+                Console.WriteLine($"Differences {dimCt} < {mtGL.Neutral().GL.N}\n");
+            
+            continue;
+        }
+
+        missing.Add(e);
+    }
+
+    var total = allMtCycSdp.Length;
+    missing.Println(e => $"M({e.Item1}x:{e.Item2}){e.Item3}", $"Missing:{missing.Count} Found:{total - missing.Count}/{total}");
+    GlobalStopWatch.Show("END");
+    Console.Beep();
+}
+
+{
+    AllGensOfMtCycSdpUpToOrder(32);
+}
