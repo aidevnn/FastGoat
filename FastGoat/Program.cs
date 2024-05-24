653b7aa1f0e2d4b89760fcd829476f56965dae0a
aidevnn <qadassi.ilyas@gmail.com>
2024-05-24 22:15:27 +0300

    Crafting Orthogonality of SL(2,3) Characters

--- FastGoat/Program.cs
+++ FastGoat/Program.cs
@@ -31,3 +31,36 @@ using GroupRegX = System.Text.RegularExpressions;
 //////////////////////////////////
 
 Console.WriteLine("Hello World");
+
+void TestOrth<T>(ConcreteGroup<T> g, bool indStab = true, params (int dim, int[] linIdx)[] infos) where T : struct, IElt<T>
+{
+    var ct = FG.CharacterTableEmpty(g);
+    ct.DerivedSubGroupLift();
+    if (indStab)
+        ct.InductionFromStabilizers();
+    
+    Logger.SetOff();
+    DisplayGroup.HeadOrders(g);
+    ct.DisplayCells(tableOnly: true);
+    if (ct.TableComplete)
+        return;
+    
+    ct.SolveSumSquare();
+    ct.SolveOrthogonality(infos);
+    ct.DisplayCells();
+    Console.Beep();
+}
+
+{
+    GlobalStopWatch.Restart();
+    TestOrth(FG.SL2p(3), infos: (2, [0, 1, 2]));
+    TestOrth(FG.GL2p(3), infos: (2, [0, 1]));
+    TestOrth(Group.SemiDirectProd(FG.SL2p(3), FG.AbelianMat(2)), infos: (2, [0, 1, 2, 3, 4, 5]));
+    TestOrth(Product.Generate(FG.SL2p(3), FG.AbelianMat(2)), infos: (2, [0, 1, 2, 3, 4, 5]));
+
+    TestOrth(FG.Quaternion(16), indStab: false, infos: (2, [0, 1]));
+    TestOrth(FG.MetaCyclicSdp(4, 4, 3), indStab: false, infos: (2, [0, 4]));
+    TestOrth(FG.DiCyclic(5), indStab: false, infos: [(2, [0, 2]), (2, [0, 2])]);
+    TestOrth(FG.DiCyclic(6), indStab: false, infos: [(2, [0, 2]), (2, [0, 2])]);
+    GlobalStopWatch.Show(); // Time:54.696s
+}
--- FastGoat/UserGroup/Characters/CharacterTable.orth.cs
+++ FastGoat/UserGroup/Characters/CharacterTable.orth.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.Numerics;
 using FastGoat.Commons;
 using FastGoat.Structures;
 using FastGoat.Structures.VecSpace;
@@ -42,33 +43,42 @@ public partial class CharacterTable<T> where T : struct, IElt<T>
     }
 
 
-    public void SolveOrthogonality()
+    public void SolveOrthogonality(params (int dim, int[] linIdx)[] infos)
     {
         PrefillOrthogonality();
-        var nbOcl = AllCharacters.Count(chi => !chi.HasAllValues);
-        if (nbOcl == 0)
+
+        var todoChis = AllCharacters.Select((chi, k) => (chi, k)).Where(e => !e.chi.HasAllValues).ToArray();
+        if (todoChis.Length == 0)
             return;
-        if (nbOcl > 3)
+        if (todoChis.Length > 3)
         {
-            Console.WriteLine($"Missing {nbOcl} characters or more");
+            Console.WriteLine($"Missing {todoChis.Length} characters or more");
             // return;
         }
+
+        // Classes.Select(e=>$"{Classes.GetClassName(e)} Stabx:{Classes.GetClassStabx(e).Count()}").Println("Stabx");
 
         var cells = new List<(int, int)>();
         for (int i = 0; i < NbClasses; i++)
         {
+            var chi = AllCharacters[i];
             for (int j = 0; j < NbClasses; j++)
             {
-                if (!AllCharacters[i][Classes.GetRepresentative(j)].HasValue)
+                var e = Classes.GetRepresentative(j);
+                if (!chi[e].HasValue)
+                {
                     cells.Add((i, j));
+                }
             }
         }
 
         var xis = Ring.Polynomial(Cnf.CnfZero, MonomOrder.Lex, (cells.Count + 1, "x"));
-        var mapCells = xis.SkipLast(1).Select((e, i) => (e, i)).ToDictionary(e => e.e, e => cells[e.i]);
-        var mapSymb = mapCells.ToDictionary(e => e.Value, e => e.Key);
+        var mapCells = cells.Take(cells.Count).Select((ij, k) => (ij, k)).ToDictionary(e => xis[e.k], e => e.ij);
+
+        var mapSymb = mapCells.GroupBy(e => e.Value)
+            .ToDictionary(e => e.Key, e => e.Select(e0 => e0.Key).Aggregate((a0, a1) => a0 + a1));
         var mapInd = mapCells.ToDictionary(e => e.Key.ExtractIndeterminate, e => e.Value);
-        var xz = xis.Last();
+        var xz = xis[cells.Count];
         var mat = new Polynomial<Cnf, Xi>[NbClasses, NbClasses];
 
         foreach (var gi in Classes)
@@ -84,50 +94,98 @@ public partial class CharacterTable<T> where T : struct, IElt<T>
             }
         }
 
-        var rg = NbClasses.Range();
-        var Ocl = Classes.ToDictionary(e => e, e => Classes.GetClassStabx(e).Count());
         Console.WriteLine();
         Console.WriteLine(Ring.Matrix2String(mat));
         Console.WriteLine();
-        
+
+        var list = new List<Polynomial<Cnf, Xi>[]>();
+        for (int i = 0; i < NbClasses; i++)
+            list.Add(NbClasses.Range().Select(j => mat[i, j]).ToArray());
+
+        var eqs = new List<Polynomial<Cnf, Xi>>();
+        var firstIdx = todoChis.Max(e => e.k);
+
+        var rmSeq = todoChis.GroupBy(e => e.chi[Gr.Neutral()]!.Value)
+            .ToDictionary(l => (int)l.Key.E[0].Num, l => l.Select(e => e.k).ToArray());
+
+        var allIdx = infos.SelectMany(l => l.linIdx).Distinct().ToArray();
+        var linChis = AllCharacters.Where(chi => chi.IsLinear)
+            .Select((chi, k) => (chi, k))
+            .Where(e => allIdx.Contains(e.k))
+            .ToDictionary(e => e.k, e => e.chi);
+
+        var infosDim = infos.GroupBy(e => e.dim).ToDictionary(e => e.Key, e => e.Select(l => l.linIdx).ToArray());
+        foreach (var (dim, linIdxs) in infosDim)
+        {
+            var rm = rmSeq[dim];
+            int nb = 0;
+            foreach (var linIdx in linIdxs)
+            {
+                var k0 = rm[nb];
+                nb += linIdx.Length;
+                foreach (var (idx, i) in linIdx.Select((idx, i) => (l0: idx, i)))
+                {
+                    var k1 = k0 + i;
+                    var linChi = linChis[idx];
+                    var pol = NbClasses.Range().Select(k => mat[k0, k] * linChi[Classes.GetRepresentative(k)]!.Value)
+                        .ToArray();
+                    foreach (var i0 in NbClasses.Range())
+                        eqs.Add(list[k1][i0] - pol[i0]);
+
+                    list[k1] = pol;
+                }
+            }
+        }
+
+        // Regular Character
+        for (int i = 1; i < NbClasses; ++i)
+        {
+            var xi0 = mat[firstIdx, i];
+            var xi1 = -NbClasses.Range()
+                .Where(k => k != firstIdx)
+                .Aggregate(xz.Zero, (sum, k) => sum + list[k][0] * list[k][i]) / list[firstIdx][0];
+
+            eqs.Add(xi0 - xi1);
+        }
+
+        Console.WriteLine();
+        Console.WriteLine(Ring.Matrix2String(Ring.Matrix(list.Count, list.SelectMany(l => l).ToArray())));
+        Console.WriteLine();
+
+        var rg = NbClasses.Range();
+        var Ocl = Classes.ToDictionary(e => e, e => Classes.GetClassStabx(e).Count());
+
         var allCombs = rg.SelectMany(i => rg.Where(j => j > i).Select(j => (i, j))).ToArray();
         var ggi = Gr.Select(e => (g: Classes.GetIndex(e), gi: Classes.GetIndex(Gr.Invert(e)))).ToArray();
-        // var clggi = rg.ToDictionary(i => i, i => IndexesTint[CClasses.GetRepresentative(Gr.Invert(Repr[i]))]);
         var clggi = rg.ToDictionary(i => i, i => Classes.GetIndex(Gr.Invert(Classes.GetRepresentative(i))));
-        
-        var eqs = new List<Polynomial<Cnf, Xi>>();
 
-        // Regular character
-        eqs.AddRange(rg.Skip(1).Select(j => rg.Aggregate(xz.Zero, (sum, i) => sum + mat[i, 0] * mat[i, j])));
-        
+        var mapXiDegDim = mapCells.ToDictionary(e => e.Key.ExtractIndeterminate,
+            e => (Classes.GetClassStabx(e.Value.Item2).Count(),
+                (int)AllCharacters[e.Value.Item1][Gr.Neutral()]!.Value.E[0].Num));
+
         // All i, Sum[g](Xi(g)Xi(g^−1))= |G|
         eqs.AddRange(rg.Select(i =>
-            ggi.Aggregate(xz.Zero, (sum, kp) => sum + mat[i, kp.g] * mat[i, kp.gi]) -
-            (xz.One * Gr.Count())));
-        
+            ggi.Aggregate(xz.Zero, (sum, kp) => sum + list[i][kp.g] * list[i][kp.gi]) - xz.One * Gr.Count()));
+
         // All i <> j, Sum[g](Xi(g)Xj(g^−1))=  0
         eqs.AddRange(allCombs.Select(e =>
             ggi.Aggregate(xz.Zero,
-                (sum, kp) => sum + mat[e.i, kp.g] * mat[e.j, kp.gi])));
-        
+                (sum, kp) => sum + list[e.i][kp.g] * list[e.j][kp.gi])));
+
         // All g, h in Cl(g), Sum[r](Xr(g)Xr(h^−1))= |Cl(g)|
         eqs.AddRange(clggi.Select(kp =>
-            rg.Aggregate(xz.Zero,
-                (sum, r) => sum + mat[r, kp.Key] * mat[r, kp.Value]) -
-            (xz.One * Ocl[Classes.GetRepresentative(kp.Key)])));
+            rg.Aggregate(xz.Zero, (sum, r) => sum + list[r][kp.Key] * list[r][kp.Value]) -
+            xz.One * Ocl[Classes.GetRepresentative(kp.Key)]));
+
         // All g, h not in Cl(g), Sum[r](Xr(g)Xr(h^−1))=  0
         eqs.AddRange(
             allCombs.Select(e => rg.Aggregate(xz.Zero,
-                (sum, r) => sum + mat[r, e.i] * mat[r, clggi[e.j]])));
-        
+                (sum, r) => sum + list[r][e.i] * list[r][clggi[e.j]])));
+
         eqs.RemoveAll(e => e.IsZero());
         eqs.Distinct().OrderBy(eq => eq.Degree).ThenBy(eq => eq.Coefs.Count).Println("System");
-        
-        // var mapCnf = mapSymb.ToDictionary(e => e.Value.ExtractIndeterminate, e => Cnf.Nth(NbStabx[e.Key.Item2]));
-        var mapCnf = mapSymb.ToDictionary(e => e.Value.ExtractIndeterminate,
-            e => Cnf.Nth(Classes.GetClassStabx(e.Key.Item2).Count()));
-        
-        var solDegreeOne = SolveDegreOne(eqs.Where(eq => eq.Degree == 1).ToArray(), mapCnf, xz);
+
+        var solDegreeOne = SolveDegreOne(eqs.Where(eq => eq.Degree == 1).ToArray(), mapXiDegDim, xz);
         var subsEq = eqs.Select(eq => solDegreeOne.Aggregate(eq, (eq0, s) => eq0.Substitute(s.Value, s.Key)))
             .Where(eq => !eq.IsZero()).Distinct().ToArray();
         subsEq.Println("Substitute Eqs");
@@ -135,31 +193,29 @@ public partial class CharacterTable<T> where T : struct, IElt<T>
         subs.Println();
         subsEq2.Println("Substitute Eqs2");
         var redEqs = Ring.ReducedGrobnerBasis(subsEq2);
-        
+
         redEqs.Println("Reduced System");
         Console.WriteLine();
-        var allSolutions = SolveSystem(solDegreeOne, redEqs, mapCnf, xz)
-            .Select(sol => ReverseSubstitution(sol, subs))
-            .ToArray();
-        foreach (var solution in allSolutions)
-        {
-            solution.Println("Solution");
-            Console.WriteLine();
-            // break;
-        }
+        var allSolutions = SolveSystem(solDegreeOne, redEqs, mapXiDegDim, xz)
+            .Select(sol => ReverseSubstitution(sol, subs));
 
-        var firstSol = allSolutions[0];
+        var firstSol =
+            allSolutions.FirstOrDefault(sol => sol.Count + 1 == xis.Length, new Dictionary<Xi, Cnf>());
+        firstSol.Println("Solutions");
+        Console.WriteLine();
         foreach (var (xi, cnf) in firstSol)
         {
+            if (!mapInd.ContainsKey(xi))
+                continue;
+
             var (i, j) = mapInd[xi];
             var chiMap = AllCharacters[i].Map.ToDictionary(kv => kv.Key, kv => kv.Value);
             chiMap[Classes.GetRepresentative(j)] = cnf;
             AllCharacters[i] = new(Classes, chiMap);
-            // Cells[i + 3, j + 2] = CnfCells[i, j] = new CnfCell(cnf);
         }
     }
 
-    private Dictionary<Xi, Cnf> SolveDegreOne(Polynomial<Cnf, Xi>[] eqs, Dictionary<Xi, Cnf> mapCnf,
+    private Dictionary<Xi, Cnf> SolveDegreOne(Polynomial<Cnf, Xi>[] eqs, Dictionary<Xi, (int, int)> mapXiDegDim,
         Polynomial<Cnf, Xi> xz)
     {
         var oneIndeterminate = eqs.Where(eq => eq.Degree == 1 && eq.NbIndeterminates == 1)
@@ -169,9 +225,8 @@ public partial class CharacterTable<T> where T : struct, IElt<T>
         var mapSol = new Dictionary<Xi, Cnf>();
         foreach (var p in oneIndeterminate)
         {
-            var ind = p.ExtractIndeterminate;
-            var cnfOrd = mapCnf[ind];
-            mapSol[ind] = Solve(p, ind, cnfOrd)[0];
+            var (ind, cs) = Solve(p, mapXiDegDim).First()[0];
+            mapSol[ind] = cs;
         }
 
         return mapSol;
@@ -183,12 +238,15 @@ public partial class CharacterTable<T> where T : struct, IElt<T>
         var lt = new List<(Xi, Polynomial<Cnf, Xi>)>();
         while (eqs0.Any(eq => eq.Degree == 1))
         {
-            var eq = eqs0.Where(eq => eq.Degree == 1).OrderBy(eq => eq.Coefs.Count).First(eq => eq.Degree == 1);
-            var xi = eq.ExtractAllIndeterminates.Order().First();
+            var eq = eqs0.Where(eq => eq.Degree == 1)
+                .OrderBy(eq => eq.Coefs.Count)
+                .ThenByDescending(eq => eq.ExtractAllIndeterminates.Order().Last())
+                .First(eq => eq.Degree == 1);
+            var xi = eq.ExtractAllIndeterminates.Order().Last();
             var ci = eq[new(eq.Indeterminates, xi)];
             var yi = -eq / ci + eq.X(xi);
             lt.Add((xi, yi));
-            Console.WriteLine(new { eq, xi, yi });
+            // Console.WriteLine(new { eq, xi, yi });
             eqs0 = eqs0.Select(eq0 => eq0.Substitute(yi, xi)).Where(eq0 => !eq0.IsZero()).Distinct().ToArray();
         }
 
@@ -204,7 +262,7 @@ public partial class CharacterTable<T> where T : struct, IElt<T>
             var s0 = subs0.Where(e => e.Item2.ExtractAllIndeterminates.ToHashSet().IsSubsetOf(mapSol.Keys)).ToArray();
             if (s0.Length == 0)
                 break;
-            
+
             var dic0 = s0.Select(e =>
                     (e.Item1, e.Item2.Substitute(mapSol.Select(f => (f.Key, f.Value * e.Item2.One)).ToList())))
                 .ToDictionary(e => e.Item1, e => e.Item2.ConstTerm);
@@ -220,29 +278,19 @@ public partial class CharacterTable<T> where T : struct, IElt<T>
     }
 
     private IEnumerable<Dictionary<Xi, Cnf>> SolveSystem(Dictionary<Xi, Cnf> solutions, Polynomial<Cnf, Xi>[] eqs,
-        Dictionary<Xi, Cnf> mapCnf, Polynomial<Cnf, Xi> xz)
+        Dictionary<Xi, (int deg, int dim)> mapXiDegDim, Polynomial<Cnf, Xi> xz)
     {
-        var oneIndeterminate = eqs.Where(eq => eq.NbIndeterminates == 1).OrderBy(eq => eq.Coefs.Keys.Max(m => m.Degree))
-            .ToArray();
-        // eqs.Println("Reduced System");
-        // oneIndeterminate.Println("One Indeterminate");
-        // Console.WriteLine();
-
-        var mapSol = new Dictionary<Xi, Cnf[]>();
-        foreach (var p in oneIndeterminate)
+        var eqFirst = eqs.OrderBy(eq => eq.NbIndeterminates)
+            .ThenBy(eq => eq.Degree)
+            .ThenBy(eq => eq.ExtractAllIndeterminates.Select(xi => mapXiDegDim[xi])
+                .Select(e => BigInteger.Pow(e.deg + 1, e.dim)).Aggregate((a0, a1) => a0 * a1))
+            .ThenBy(eq => eq.Coefs.Count())
+            .First();
+        foreach (var pos in Solve(eqFirst, mapXiDegDim))
         {
-            var ind = p.ExtractIndeterminate;
-            var cnfOrd = mapCnf[ind];
-            mapSol[ind] = Solve(p, ind, cnfOrd);
-        }
+            if (pos.Length == 0)
+                yield break;
 
-        // mapSol.Select(e => $"{e.Key} in {{{e.Value.Glue("; ")}}}").Println("Solutions One Indeterminates");
-
-        var allPos = mapSol.SelectMany(e => e.Value.Select(c => (e.Key, c * xz.One))).GroupBy(e => e.Item1)
-            .Select(e => e.ToArray()).ToArray();
-
-        foreach (var pos in allPos.MultiLoop())
-        {
             var pos0 = pos.ToArray();
             pos0.Println("Possibility");
             var subsEq = eqs.Select(eq => pos0.Aggregate(eq, (eq0, s) => eq0.Substitute(s.Item2, s.Item1)))
@@ -250,7 +298,7 @@ public partial class CharacterTable<T> where T : struct, IElt<T>
 
             var nsol = new Dictionary<Xi, Cnf>(solutions);
             foreach (var kp in pos0)
-                nsol.Add(kp.Key, kp.Item2.Coefs.Values.First());
+                nsol.Add(kp.Item1, kp.Item2);
 
             if (subsEq.Length == 0)
             {
@@ -266,7 +314,7 @@ public partial class CharacterTable<T> where T : struct, IElt<T>
                     yield break;
                 }
 
-                foreach (var nsol0 in SolveSystem(nsol, subsEq, mapCnf, xz))
+                foreach (var nsol0 in SolveSystem(nsol, subsEq, mapXiDegDim, xz))
                 {
                     yield return nsol0;
                 }
@@ -279,7 +327,78 @@ public partial class CharacterTable<T> where T : struct, IElt<T>
         }
     }
 
-    private Cnf[] Solve(Polynomial<Cnf, Xi> p, Xi ind, Cnf c)
+    private IEnumerable<(Xi xi, Cnf)[]> Solve(Polynomial<Cnf, Xi> p, Dictionary<Xi, (int deg, int dim)> mapXi)
+    {
+        var xis = p.ExtractAllIndeterminates;
+        if (xis.Length == 1 && p.Degree == 1)
+        {
+            List<Cnf> algSol = new();
+            var ind = xis[0];
+            var (deg, dim) = mapXi[ind];
+            var c = Cnf.Nth(deg);
+            var f0 = p.ToKPoly(ind);
+            var sol = -f0[0] / f0[1];
+            algSol.Add(sol * c.One);
+            return algSol.Select(c0 => new[] { (ind, c0) });
+        }
+
+        if (xis.Length == 1 && p.Degree == 2)
+        {
+            var ind = xis[0];
+            var (deg, dim) = mapXi[ind];
+            var c = Cnf.Nth(deg).Simplify();
+            var f0 = p.ToKPoly(ind);
+            if (!f0.Coefs.Any(c0 => c0.E.Poly.Degree > 0))
+            {
+                var x = FG.QPoly();
+                var f = p.ToKPoly(ind).Coefs.Select((c0, i) => c0.E[0] * x.Pow(i))
+                    .Aggregate(x.Zero, (sum, xi) => sum + xi);
+
+                var X = FG.KPoly('X', c.E);
+                var P = f.Substitute(X);
+                Console.WriteLine($"Factors of {P} in splitting field {c.E.F} of Q({c})[x]");
+                var pows = IntExt.Coprimes(deg).ToArray();
+                var aroots = IntFactorisation.AlgebraicRoots(P);
+                var roots = aroots.SelectMany(r => pows.Select(c0 => new Cnf(deg, r.Poly.Substitute(c.E.Pow(c0))))
+                        .Where(cr => p.Substitute(cr, ind).IsZero()))
+                    .Distinct()
+                    .ToArray();
+                roots.Select(r => (ind, r, p.Substitute(r, ind).IsZero())).Println($"Nb possibilities:{roots.Length}");
+                return roots.Select(r => new[] { (ind, r) });
+            }
+            else
+            {
+                Console.WriteLine($"############# Faillure to solve {p} = 0");
+            }
+        }
+
+        if (xis.Length > 0)
+        {
+            var cidim = xis.Select(xi => (xi, mapXi[xi])).Select(e => (e.xi, e.Item2.deg, e.Item2.dim))
+                .ToArray();
+
+            var seqs = cidim.Select(e => (e.xi, e.deg, e.dim,
+                    seq: e.deg.Range().Select(k => Cnf.Nth(e.deg).Pow(k).Simplify()).Append(Cnf.CnfZero).Order()
+                        .ToHashSet()))
+                .ToArray();
+
+            var total = seqs.Select(e => BigInteger.Pow(e.deg + 1, e.dim)).Aggregate((a0, a1) => a0 * a1);
+            seqs.Select(e => (e.xi, e.dim, e.deg, e.seq.Count)).Println($"Nb possibilities:{total}");
+
+            var xseqs = seqs.Select(e => e.seq.MultiLoop(e.dim).Select(l => l.Aggregate((a0, a1) => a0 + a1))
+                    .Distinct()
+                    .Select(c => (e.xi, e.deg, e.dim, tr: c.Simplify()))
+                    .ToArray())
+                .ToArray();
+
+            return xseqs.MultiLoop().Where(l => l.Aggregate(p, (p0, e) => p0.Substitute(e.tr, e.xi)).IsZero())
+                .Select(l => l.Select(l0 => (l0.xi, l0.tr.Simplify())).ToArray());
+        }
+
+        throw new();
+    }
+
+    private Cnf[] SolveBak(Polynomial<Cnf, Xi> p, Xi ind, Cnf c, int dim)
     {
         List<Cnf> algSol = new();
         var f0 = p.ToKPoly(ind);
