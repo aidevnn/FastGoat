7d002e8912c899b80ffb40b78a03a8bbf855d1eb
aidevnn <qadassi.ilyas@gmail.com>
2024-12-03 05:53:18 +0300

    Crafting LLL for gaussian integers, more examples

--- FastGoat/Program.cs
+++ FastGoat/Program.cs
@@ -69,6 +69,11 @@ Console.WriteLine("Hello World");
     return (KMatrix<K>.MergeSameRows(vs), new(u));
 }
 
+K InnerProduct<K>(IEnumerable<K> A, IEnumerable<K> B) where K : struct, IElt<K>, IRingElt<K>, IFieldElt<K>, IFloatElt<K>
+{
+    return A.Zip(B).Select(e => e.First * e.Second.Conj).Aggregate((a0, a1) => a0 + a1);
+}
+
 KMatrix<K> LLL<K>(KMatrix<K> A) where K : struct, IElt<K>, IRingElt<K>, IFieldElt<K>, IFloatElt<K>
 {
     var n = A.N;
@@ -77,6 +82,7 @@ KMatrix<K> LLL<K>(KMatrix<K> A) where K : struct, IElt<K>, IRingElt<K>, IFieldEl
     var ws = Ws.Cols;
     var N = new KMatrix<K>(M.Coefs);
     int i = 1;
+    var d = A.KOne / 2;
     while (i < n)
     {
         var wi = w[i];
@@ -105,10 +111,10 @@ KMatrix<K> LLL<K>(KMatrix<K> A) where K : struct, IElt<K>, IRingElt<K>, IFieldEl
                 wsi2 += wsi.Coefs[k, 0].Absolute2;
             }
 
-            if (K.Abs(wsip2).CompareTo(K.Abs(2 * wsi2)) > 0)
+            var a = N[i, i - 1];
+            if (K.Abs(wsip2) > 2 * K.Abs(wsi2))
             {
-                var a = N[i, i - 1];
-                var b = a * wsip2 / (wsi2 + a.Absolute2 * wsip2);
+                var b = (a * wsip2 / (wsi2 + a.Absolute2 * wsip2)).Conj; // Solve everything
                 for (int k = 0; k < n; k++)
                 {
                     var tmp = wsi.Coefs[k, 0] + a * wsip.Coefs[k, 0];
@@ -135,41 +141,41 @@ KMatrix<K> LLL<K>(KMatrix<K> A) where K : struct, IElt<K>, IRingElt<K>, IFieldEl
     return KMatrix<K>.MergeSameRows(w);
 }
 
-BigCplx[] AlphaBetaPolynomial(BigCplx alpha, BigCplx beta, int d, int O)
+(BigCplx[] col, BigCplx sum)[] AlphaBetaPolynomial(BigCplx alpha, BigCplx beta, int d)
 {
     if (Logger.Level != LogLevel.Off)
         Console.WriteLine("Start LLL algorithm");
     
-    var N = BigCplx.FromBigReal(BigReal.Pow10(O, O));
-    var mat = new KMatrix<BigCplx>(N.Zero, d, d).Zero;
+    var z = BigCplx.BcZero(alpha.O);
+    var N = BigCplx.FromBigReal(BigReal.Pow10(alpha.O - 10, alpha.O));
+    var mat = new KMatrix<BigCplx>(z.Zero, d, d).Zero;
     var ai = alpha.One;
     var aipow = new List<BigCplx>();
     for (int i = 0; i < mat.M - 1; i++)
     {
         aipow.Add(ai);
-        mat.Coefs[i, i] = N.One;
+        mat.Coefs[i, i] = z.One;
         mat.Coefs[i, mat.N - 1] = (ai * N).RoundEven;
         ai *= alpha;
     }
-    
-    mat.Coefs[mat.N - 1, mat.N - 1] = (beta * N).RoundEven;
-        
-    var lll = IntFactorisation.LLL(mat.T);
+
+    mat.Coefs[mat.M - 1, mat.N - 1] = (beta * N).RoundEven;
+
+    var lll = LLL(mat.T);
 
     Console.WriteLine(mat);
     Console.WriteLine();
     Console.WriteLine(lll);
 
-    var col = lll.Cols
-        .OrderBy(l => l.SkipLast(1).Zip(aipow).Aggregate(-beta, (acc, v) => acc + v.First * v.Second).Magnitude2)
-        .First();
-    
+    var cols = lll.Cols
+        .Select(
+            l => (l, sum: l.SkipLast(1).Zip(aipow).Aggregate(z.Zero, (acc, v) => acc + v.First * v.Second) / beta)
+        )
+        .OrderBy(e => e.l.SkipLast(1).Aggregate(z.Zero, (acc, f) => acc + f.Absolute2))
+        .Where(e => BigCplx.Abs(e.sum) < double.Exp10(alpha.O / 10.0) && !e.sum.ToBigCplx(alpha.O / 2).IsZero() && e.sum.IsGaussian())
+        .ToArray();
     Console.WriteLine("End LLL algorithm");
-    Console.WriteLine("Possible Solution");
-    Console.WriteLine(col.T);
-    Console.WriteLine();
-
-    return col.SkipLast(1).Select(c => -c.RoundEven).ToArray();
+    return cols.Select(e => (col: e.l.SkipLast(1).Select(c => c.RoundEven).ToArray(), e.sum.RoundEven)).ToArray();
 }
 
 void TestGramSchimdt()
@@ -260,7 +266,6 @@ void TestLLL()
         Console.WriteLine(IntFactorisation.LLL(A));
         Console.WriteLine("LLL2");
         Console.WriteLine(LLL(A));
-        Console.WriteLine();
     }
 
     {
@@ -283,40 +288,94 @@ void TestLLL()
     }
 }
 
-void ExamplePol()
+void ExamplePol(KPoly<Rational> P, int O)
 {
     Ring.DisplayPolynomial = MonomDisplay.StarCaret;
-    var x = FG.QPoly();
-    var P = x.Pow(6) + 108;
     Console.WriteLine(P);
-
-    var O1 = 50; // rounding digits
-    var O2 = 60; // maximum precision digits
-
-    var nRoots = FG.NRoots(P.ToBcPoly(O2));
+    var (X, a) = FG.EPolyXc(P, 'a');
+    var l = IntFactorisation.AlgebraicRoots(X.Pow(2) + 1);
+    var nRoots = FG.NRoots(P.ToBcPoly(O));
     nRoots.Println();
     var alpha = nRoots[0];
-    Logger.Level = LogLevel.Level1;
-    foreach (var beta in nRoots.Skip(1))
+    Console.WriteLine($"alpha={alpha}");
+
+    var Qi = l.Count != 0;
+    Console.WriteLine($"Qi {Qi}");
+    var I = Qi ? l.First(e => e.Poly.Substitute(alpha).Equals(alpha.I)) : a.Zero;
+    Console.WriteLine($"{I} {I * I} {I.Poly.Substitute(alpha)}");
+
+    EPoly<Rational> Algebraic(BigCplx e) => e.RealPart.ToRational + e.ImaginaryPart.ToRational * I;
+    var set = new HashSet<EPoly<Rational>>() { a };
+    var If = alpha.I;
+    var eqCplx = EqualityComparer<BigCplx>.Create(
+        (a0, a1) => (a0 - a1).IsZero4d(), a0 => a0.O
+    );
+    var remRoots = nRoots.Skip(1).ToHashSet(eqCplx);
+    var dicRoots = nRoots.ToDictionary(e => e.ToBigCplx(O / 2), e => e, eqCplx);
+    var alphaOpp = (-alpha).ToBigCplx(O / 2);
+    if (dicRoots.ContainsKey(alphaOpp))
     {
-        var rel = AlphaBetaPolynomial(alpha, beta, P.Degree + 1, O1);
-        var sum = rel.Select((c, i) => c * alpha.Pow(i)).Aggregate((a0, a1) => a0 + a1);
-        Console.WriteLine($"alpha={alpha}");
-        var fact = (sum / beta).ToBigCplx(O1 - 10);
-        var test = fact.IsReal() && (fact.RealPart - fact.RealPart.RoundEven).IsZero4d();
-        Console.WriteLine($"beta={beta} sum={sum} fact={fact}");
-        var r = fact.RealPart.RoundEven.ToRational;
-        var P1 = rel.Select((c, i) => c.RealPart.ToRational * x.Pow(i)).Aggregate((a0, a1) => a0 + a1) / r;
-        if (test)
-            Console.WriteLine($"beta = P1(alpha) with P1={P1}");
-        else
-            Console.WriteLine("#### Problem");
-        Console.WriteLine();
+        remRoots.Remove(dicRoots[alphaOpp]);
+        set.Add(-a);
     }
+    
+    while(true)
+    {
+        var sz = 0;
+        Console.WriteLine($"remRoots:{remRoots.Count}");
+        if (remRoots.Count == 0)
+            break;
+        
+        var beta = remRoots.First();
+        var rels = AlphaBetaPolynomial(alpha, beta, P.Degree + 1);
+        Console.WriteLine($"Possibles Solutions:{rels.Length}");
+        foreach (var cs in rels)
+        {
+            var (col, sum) = cs;
+            if (sum.RealPart.IsZero())
+            {
+                sum /= If;
+                col = col.Select(e => e / If).ToArray();
+            }
+            
+            var sumAlg = Algebraic(sum);
+            var relAlg = col.Select((c, i) => a.Pow(i) * Algebraic(c)).Aggregate((c0, c1) => c0 + c1);
+            var betaAlg = relAlg / sumAlg;
+            var Pbeta = P.Substitute(betaAlg);
+            if (Pbeta.IsZero())
+            {
+                Console.WriteLine($"[{col.Glue(",")}]");
+                Console.WriteLine(sum);
+                Console.WriteLine($"beta={betaAlg}");
+                Console.WriteLine($"P(beta)={Pbeta}");
+                Console.WriteLine();
+                set.UnionWith(Group.KAut(set.Append(betaAlg).ToArray()).Select(e => e.E));
+                remRoots.ExceptWith(set.Select(e => e.Poly.Substitute(alpha).ToBigCplx(O / 2))
+                    .Select(e => dicRoots[e]));
+            }
+        }
+    }
+    
+    var P2 = set.Select(e => X - e).Aggregate((x0, x1) => x0 * x1);
+    if (!P2.Equals(P.Substitute(X)))
+        throw new();
+    
+    DisplayGroup.HeadElementsNames(Group.KAut(set.ToArray()));
+    Console.WriteLine();
 }
 
 {
-    TestGramSchimdt();
-    TestLLL();
-    ExamplePol();
+    // TestGramSchimdt();
+    // TestLLL();
+
+    var x = FG.QPoly();
+    ExamplePol(x.Pow(4) - 2 * x.Pow(2) + 9, 20); // C2 x C2
+    ExamplePol(x.Pow(4) + x.Pow(3) + x.Pow(2) + x + 1, 20); // C4
+    ExamplePol(x.Pow(6) + 108, 20); // S3
+    ExamplePol(x.Pow(8) + 4 * x.Pow(6) + 2 * x.Pow(4) + 28 * x.Pow(2) + 1, 30); // D8
+    
+    ExamplePol(x.Pow(10) + 10 * x.Pow(8) + 125 * x.Pow(6) + 500 * x.Pow(4) + 2500 * x.Pow(2) + 4000, 60); // D10
+    ExamplePol(x.Pow(12) + 6 * x.Pow(8) + 26 * x.Pow(6) - 63 * x.Pow(4) + 162 * x.Pow(2) + 81, 100); // A4
+    ExamplePol(x.Pow(18) + 171 * x.Pow(12) + 5130 * x.Pow(6) + 27, 120); // C3 x: C6
+    ExamplePol(x.Pow(20) + 2500 * x.Pow(10) + 50000, 120); // C5 x: C4
 }
