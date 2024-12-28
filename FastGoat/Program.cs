7a0e035141249343d897a023a0df63d1d2794e72
aidevnn <qadassi.ilyas@gmail.com>
2024-12-29 00:18:28 +0300

    Cleaning worktable

--- FastGoat/Program.cs
+++ FastGoat/Program.cs
@@ -1,12 +1,4 @@
-﻿using System.Numerics;
-using FastGoat.Commons;
-using FastGoat.Structures;
-using FastGoat.Structures.VecSpace;
-using FastGoat.UserGroup;
-using FastGoat.UserGroup.Integers;
-using FastGoat.UserGroup.Lattice;
-using Rq = FastGoat.Structures.VecSpace.KPoly<FastGoat.UserGroup.Integers.Rational>;
-
+﻿
 //////////////////////////////////
 //                              //
 //    Work Table for crafting   //
