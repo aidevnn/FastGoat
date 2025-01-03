1bc31b630dd9f78e6a2fd5f2b6e371f7e1daa088
aidevnn <qadassi.ilyas@gmail.com>
2025-01-04 00:46:23 +0300

    Reg24 parameters. fix #67

--- FastGoat/UserGroup/Lattice/Regev.cs
+++ FastGoat/UserGroup/Lattice/Regev.cs
@@ -16,7 +16,7 @@ namespace FastGoat.UserGroup.Lattice;
 public class Regev
 {
     public int N { get; }
-    public int Q { get; }
+    public int P { get; }
     public int M { get; }
     public double A { get; }
     private Vec<ZnInt64> SK { get; }
@@ -27,44 +27,45 @@ public class Regev
     {
         // All conditions
         
-        // 1) m = (1+ε)*n*log2(n) ε=1
-        var m = (int)(2 * n * double.Log2(n));
+        // 1) lim a(n)*log2(n)*√n = lim 1/log2(n) = 0
+        var a = 1.0 / (double.Log2(n) * double.Log2(n) * double.Sqrt(n));
         
-        // 2) lim a(n)*log2(n)*√n = lim 1 / (4*log10(n)) = 0
-        var a = 1.0 / (4 * double.Log10(n) * double.Log2(n) * double.Sqrt(n));
+        // 2) a*p > 2*√n
+        var p = Primes10000.First(p => a * p > 2 * double.Sqrt(n));
         
-        // 3) a*q > 2*√n
-        var q = Primes10000.First(q => a * q > 2 * double.Sqrt(n));
-        (N, Q, M, A) = (n, q, m, a);
+        // 3) m = (1+ε)*n*log2(p) ε=1
+        var m = (int)(2 * n * double.Log2(p));
+        (N, P, M, A) = (n, p, m, a);
         
-        // 4) Discrete gaussian sample with s = a*q
-        var err = Err = DiscGauss(m, q, s: a * q);
+        // 4) Discrete gaussian sample with s = a*p
+        var err = Err = DiscGauss(m, p, s: a * p);
 
-        var sk = SK = Unif(n, q);
-        var ai = m.SeqLazy().Select(_ => Unif(n, q)).ToArray();
+        var sk = SK = Unif(n, p);
+        var ai = m.SeqLazy().Select(_ => Unif(n, p)).ToArray();
         var b = ai.Zip(err).Select(e => (e.First * sk).Sum() + e.Second).ToVec();
         PK = (ai, b);
     }
 
     public RegevCipher EncryptBit(int m)
     {
+        var _m = m == 0 ? 0 : 1;
+        var acc0 = (a: PK.A[0].Zero, b: new ZnInt64(P, _m * (P / 2)));
+        
         // 5) Set S ⊂ [0..M-1] 
-        var r = DistributionExt.DiceSample(M, [true, false]).Zip(M.Range())
-            .Where(e => e.First).Select(e => e.Second).ToArray();
-        var m1 = m == 0 ? 0 : 1;
-        var acc0 = (a: PK.A[0].Zero, b: new ZnInt64(Q, m1 * (Q / 2)));
-        return r.Select(e => (a: PK.A[e], b: PK.B[e])).Aggregate(acc0, (acc, e) => (acc.a + e.a, acc.b + e.b));
+        return DistributionExt.DiceSample(M, [true, false]).Zip(M.Range())
+            .Where(e => e.First)
+            .Select(e => (a: PK.A[e.Second], b: PK.B[e.Second]))
+            .Aggregate(acc0, (acc, e) => (acc.a + e.a, acc.b + e.b));
     }
 
     public int DecryptBit(RegevCipher cipher)
     {
-        // 6) b - <s,a> distance to 0 and to Q/2
+        // 6) b - <s,a> distance to 0 and to P/2
         var d = cipher.B - (cipher.A * SK).Sum();
-        return long.Abs(d.Signed) < Q / 4 ? 0 : 1;
+        return long.Abs(d.Signed) < P / 4 ? 0 : 1;
     }
 
-    public string Params =>
-        $"Regev N:{N,-4} Q:{Q,-6} M:{M,-6} a:{A:F4} aq:{A * Q:F4} Q/M:{Q / M}";
+    public string Params => $"Regev N:{N,-4} P:{P,-6} M:{M,-6} A:{A:F4} A*P:{A * P:F4}";
 
     public void Show()
     {
