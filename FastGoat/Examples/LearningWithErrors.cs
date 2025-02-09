58eb9fedd9bb86dab653f2fa490eb2b2b4ad4d14
aidevnn <qadassi.ilyas@gmail.com>
2025-02-09 22:55:05 +0300

    Example leveled BGV

--- FastGoat/Examples/LearningWithErrors.cs
+++ FastGoat/Examples/LearningWithErrors.cs
@@ -117,6 +117,170 @@ public static class LearningWithErrors
         Console.WriteLine();
     }
 
+    static void SymbBGV()
+    {
+        var ind = new Indeterminates<Xi>(MonomOrder.GrLex, new Xi("k"), new Xi("T"), new Xi("X"), new Xi("sk"));
+        ind.ExtendAppend("epk", "pkb", "erlk", "rlkb");
+        ind.ExtendAppend("m1", "u1", "e1a", "e1b");
+        ind.ExtendAppend("m2", "u2", "e2a", "e2b");
+        ind.ExtendAppend("b");
+
+        var z = new Polynomial<Rational, Xi>(ind, Rational.KZero());
+        var Vars = z.Variables;
+        var (k, t, X, sk) = Vars.Take(4).Deconstruct();
+        var (epk, pkb, erlk, rlkb) = Vars.Skip(4).Take(4).Deconstruct();
+        var pka = (t * epk + pkb * sk);
+        var b = Vars.Last();
+        var p = b * t + 1;
+        var rlka = (t * erlk + rlkb * sk + p * sk.Pow(2));
+
+        Console.WriteLine("##### Encryption/Decryption #####");
+        Console.WriteLine("Decrypt pk: c0pk - sk * c1pk       = {0}", pka - sk * pkb);
+        Console.WriteLine("Decrypt pk: c0pk - sk * c1pk mod t = {0}", (pka - sk * pkb).Div(t).rem);
+        Console.WriteLine();
+
+        Console.WriteLine("Decrypt rlk: rlka - sk * rlkb       = {0}", rlka - sk * rlkb);
+        Console.WriteLine("Decrypt rlk: rlka - sk * rlkb mod t = {0}", (rlka - sk * rlkb).Div(t).rem);
+        Console.WriteLine("                             sk^2   = {0}", sk.Pow(2));
+        Console.WriteLine();
+
+        var (m1, u1, e1a, e1b) = Vars.Skip(8).Take(4).Deconstruct();
+        var m1a = m1 + t * e1a + u1 * pka;
+        var m1b = t * e1b + u1 * pkb;
+        Console.WriteLine($"m1a = {m1a}");
+        Console.WriteLine($"m1b = {m1b}");
+        Console.WriteLine($"Decrypt m1: m1a - sk*m1b       = {m1a - sk * m1b}");
+        Console.WriteLine($"Decrypt m1: m1a - sk*m1b mod t = {(m1a - sk * m1b).Div(t).rem}");
+        Console.WriteLine();
+
+        var (m2, u2, e2a, e2b) = Vars.Skip(12).Take(4).Deconstruct();
+        var m2a = m2 + t * e2a + u2 * pka;
+        var m2b = t * e2b + u2 * pkb;
+        Console.WriteLine($"m2a = {m2a}");
+        Console.WriteLine($"m2b = {m2b}");
+        Console.WriteLine($"Decrypt m2: m2a - sk*m1b       = {m2a - sk * m2b}");
+        Console.WriteLine($"Decrypt m2: m2a - sk*m2b mod t = {(m2a - sk * m2b).Div(t).rem}");
+        Console.WriteLine();
+
+        var add_m1m2 = m1 + m2;
+        var add_m1m2a = m1a + m2a;
+        var add_m1m2b = m1b + m2b;
+        Console.WriteLine("##### Homomorphic Addition of Cipher #####");
+        Console.WriteLine($"add_m1m2a = {add_m1m2a}");
+        Console.WriteLine($"add_m1m2b = {add_m1m2b}");
+        Console.WriteLine($"Decrypt add_m1m2: add_m1m2a - sk*add_m1m2b       = {add_m1m2a - sk * add_m1m2b}");
+        Console.WriteLine(
+            $"Decrypt add_m1m2: add_m1m2a - sk*add_m1m2b mod t = {(add_m1m2a - sk * add_m1m2b).Div(t).rem}");
+        Console.WriteLine($"                                  add_m1m2       = {add_m1m2}");
+        Console.WriteLine();
+
+        var mul_km1 = k * m1;
+        var mul_km1a = k * m1a;
+        var mul_km1b = k * m1b;
+        Console.WriteLine("##### Homomorphic Multiplication by Scalar #####");
+        Console.WriteLine($"mul_km1a = {mul_km1a}");
+        Console.WriteLine($"mul_km1b = {mul_km1b}");
+        Console.WriteLine($"Decrypt mul_km1: mul_km1a - sk*mul_km1b       = {mul_km1a - sk * mul_km1b}");
+        Console.WriteLine($"Decrypt mul_km1: mul_km1a - sk*mul_km1b mod t = {(mul_km1a - sk * mul_km1b).Div(t).rem}");
+        Console.WriteLine($"                                mul_km1       = {mul_km1}");
+        Console.WriteLine();
+
+        var mul_m1m2 = m1 * m2;
+
+        var c0 = m1a * m2a;
+        var c1 = m1a * m2b + m1b * m2a;
+        var c2 = m1b * m2b;
+
+        var mul_m1m2a = p * c0 + c2 * rlka;
+        var mul_m1m2b = p * c1 + c2 * rlkb;
+
+        Console.WriteLine("##### Homomorphic Multiplication of Ciphers #####");
+        Console.WriteLine($"mul_m1m2a = {mul_m1m2a}");
+        Console.WriteLine($"mul_m1m2a mod p = {mul_m1m2a.Div(p).rem}");
+        Console.WriteLine($"mul_m1m2b = {mul_m1m2b}");
+        Console.WriteLine();
+        Console.WriteLine($"Decrypt mul_m1m2: mul_m1m2a - sk*mul_m1m2b       = {mul_m1m2a - sk * mul_m1m2b}");
+        Console.WriteLine();
+        Console.WriteLine(
+            $"Decrypt mul_m1m2: mul_m1m2a - sk*mul_m1m2b mod t = {(mul_m1m2a - sk * mul_m1m2b).Div(t).rem}");
+        Console.WriteLine($"                                      mul_m1m2   = {mul_m1m2}");
+        Console.WriteLine();
+
+        var ska = sk + t * e1a + u1 * pka;
+        var skb = t * e1b + u1 * pkb;
+        var swk_m2a = m2a - m2b * ska;
+        var swk_m2b = -m2b * skb;
+        Console.WriteLine("##### Switch key #####");
+        Console.WriteLine($"swk_m2a = {swk_m2a}");
+        Console.WriteLine($"swk_m2b = {swk_m2b}");
+        Console.WriteLine($"Decrypt swk_m2: swk_m2a - sk*swk_m2b       = {swk_m2a - sk * swk_m2b}");
+        Console.WriteLine($"Decrypt swk_m2: swk_m2a - sk*swk_m2b mod t = {(swk_m2a - sk * swk_m2b).Div(t).rem}");
+        Console.WriteLine($"                                     m2    = {m2}");
+        Console.WriteLine();
+    }
+
+    public static void RunLeveledBGV(int N, int t0, int level, bool differentPrimes = true)
+    {
+        var sk = RLWE.SKBGV(N / 2);
+        var (pm, _, t, primes, pk, swks) = RLWE.SetupBGV(N, t0, level, sk, differentPrimes);
+        Console.WriteLine($"pm = {pm} T = {t} Primes = [{primes.Glue(", ")}]");
+        Console.WriteLine($"sk = {sk}");
+        Console.WriteLine($"pk => {pk.Params}");
+
+        foreach (var swk in swks)
+            Console.WriteLine($"swk[{swk.Key}] => {swk.Value.skPow[0].Params}");
+
+        Console.WriteLine();
+
+        var size = 1 << level;
+        var n = pm.Degree;
+
+        var seqMsg = size.SeqLazy().Select(_ => RLWE.GenUnif(n, t)).ToArray();
+        var seqCipher = seqMsg.Select(m => RLWE.EncryptBGV(m, pk)).ToArray();
+        var mul = seqMsg.Aggregate((xi, xj) => (xi * xj).ResModSigned(pm, t));
+
+        var qMul = new Queue<RLWECipher>(seqCipher);
+        while (qMul.Count > 1)
+        {
+            var c1 = qMul.Dequeue();
+            var c2 = qMul.Dequeue();
+            var (nextMod, swk) = swks[c1.Q];
+            var c1c2 = RLWE.MulRelinBGV(c1, c2, swk[2]).ModSwitch(nextMod);
+            qMul.Enqueue(c1c2);
+        }
+
+        var cMul = qMul.Dequeue();
+        var d_mul = RLWE.DecryptBGV(cMul, sk);
+
+        if (size < 17)
+            seqMsg.Println($"level:{level} Size:{size}");
+        else
+        {
+            seqMsg.Take(8).Println($"level:{level} Size:{size}");
+            seqMsg.TakeLast(8).Println("    ...");
+        }
+
+        Console.WriteLine(" *  {0}", Enumerable.Repeat('-', seqMsg.Max(l => $"{l}".Length)).Glue());
+        Console.WriteLine($" =  {mul}");
+        Console.WriteLine($"    {d_mul}");
+        if (!d_mul.Equals(mul))
+            throw new("fail");
+    }
+
+    public static void RunLeveledBGV(int N, int t0, int level, bool differentPrimes = true, int nbTests = 500)
+    {
+        GlobalStopWatch.AddLap();
+        for (int i = 0; i < nbTests; i++)
+        {
+            Console.WriteLine($"Test[{i + 1}]");
+            RunLeveledBGV(N, t0, level, differentPrimes);
+            Console.WriteLine();
+        }
+
+        GlobalStopWatch.Show($"Pass {nbTests} tests. N={N} T={t0} Level={level}.");
+        Console.WriteLine();
+    }
+
     public static void Example1Regev()
     {
         GlobalStopWatch.Restart();
@@ -191,11 +355,11 @@ public static class LearningWithErrors
         Console.WriteLine();
         if (!d_add.Equals(add_m1m2))
             throw new("m1 + m2");
-        
+
         var km1 = (k * m1).ResModSigned(pm, t);
         var ke1 = k * e1;
         var d_k1 = RLWE.DecryptBGV(ke1, sk);
-        
+
         ke1.Show("k * e1");
         Console.WriteLine($"k               = {k}");
         Console.WriteLine($"k * m1          = {km1}");
@@ -401,9 +565,9 @@ public static class LearningWithErrors
         // RLWE from RegevCipher Errors:  17 --> 135
         // 
     }
-    
+
     #endregion
-    
+
     public static void Example9WrongParameters()
     {
         for (int k = 2; k < 8; k++)
@@ -416,15 +580,15 @@ public static class LearningWithErrors
             var t = new Rational(t0);
             var sk = 10000.SeqLazy().Select(_ => RLWE.GenTernary(n))
                 .First(s => !s[n - 1].IsZero() && s.Coefs.Count(e => e.IsZero()) <= n / 4);
-        
+
             var epk = RLWE.GenDiscrGauss(n);
             var c1pk = RLWE.GenUnif(n, q0);
             var c0pk = (t * epk + c1pk * sk).ResModSigned(pm, q0);
             var pk = new RLWECipher(c0pk, c1pk, pm, t, q0);
-    
+
             var pka = pk.A.ToZnPoly(t0);
             var pkb = pk.B.ToZnPoly(t0);
-    
+
             var a = FG.EPoly(pka.X.Pow(n) + 1, 'a');
             Console.WriteLine(new { n, t, q0, pm });
             var sk1 = sk.ToZnPoly(t0).Substitute(a);
@@ -435,5 +599,12 @@ public static class LearningWithErrors
             Console.WriteLine($"Invalid:{sk1.Equals(sk2)}");
             Console.WriteLine();
         }
+    }
+
+    public static void Example10LeveledBGV()
+    {
+        IntExt.RecomputeAllPrimesUpTo(1500000);
+
+        RunLeveledBGV(16, 769, 6, true, 5);
     }
 }
