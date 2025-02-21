19548257fafad6a50f903c2a5c5e3cc2ae5a8797
aidevnn <qadassi.ilyas@gmail.com>
2025-02-21 17:24:00 +0300

    Fix constructor default parameter, xor with one level, typo

--- FastGoat/UserGroup/LWE/RLWE.cs
+++ FastGoat/UserGroup/LWE/RLWE.cs
@@ -21,7 +21,7 @@ public partial class RLWE
     public Rational T { get; }
 
     public Rational[] Primes { get; }
-    
+
     public Rational SP { get; }
 
     public RLWECipher PK { get; }
@@ -45,7 +45,7 @@ public partial class RLWE
         (PM, SK, T, Primes, SP, PK, RLKS) = SetupBGV(N, Level, differentPrimes: true);
     }
 
-    public RLWE(int N, int level, bool bootstrappingMode = false)
+    public RLWE(int N, int level, bool bootstrappingMode)
     {
         (this.N, n, Level) = (N, IntExt.Phi(N), level);
         (PM, SK, T, Primes, SP, PK, RLKS) = SetupBGV(N, Level, differentPrimes: true);
@@ -92,6 +92,7 @@ public partial class RLWE
             return $"LWEParameters(n={n}, q={T}, Xs=T({d}, n={n}), Xe=D({Sigma(n, T):f4}), m=+oo, tag='fg{n}')";
         }
     }
+
     public void Deconstruct(out int _n, out Rq pm, out Rq sk, out Rational t, out Rational q, out RLWECipher pk,
         out RLWECipher rlk)
     {
@@ -178,8 +179,6 @@ public partial class RLWE
 
     public RLWECipher NOT(RLWECipher cipher) => Thalf - cipher;
 
-    public RLWECipher[] NOT(RLWECipher[] ciphers) => ciphers.Select(NOT).ToArray();
-
     public RLWECipher AND(RLWECipher cipher1, RLWECipher cipher2)
     {
         var (c1, c2) = (Bootstrapping(cipher1), Bootstrapping(cipher2));
@@ -198,24 +197,24 @@ public partial class RLWE
     public RLWECipher OR(RLWECipher cipher1, RLWECipher cipher2) => NOT(NOR(cipher1, cipher2));
 
     public RLWECipher XOR(RLWECipher cipher1, RLWECipher cipher2) =>
-        OR(AND(cipher1, NOT(cipher2)), AND(NOT(cipher1), cipher2));
+        AND(cipher1, NOT(cipher2)) + AND(NOT(cipher1), cipher2);
+
+    public RLWECipher[] NOT(RLWECipher[] ciphers) => ciphers.Select(NOT).ToArray();
 
     public RLWECipher[] AND(RLWECipher[] ciphers1, RLWECipher[] ciphers2)
     {
         return ciphers1.Zip(ciphers2).Select(e => AND(e.First, e.Second)).ToArray();
     }
 
-    public RLWECipher[] NAND(RLWECipher[] cipher1, RLWECipher[] cipher2) => NOT(AND(cipher1, cipher2));
+    public RLWECipher[] NAND(RLWECipher[] ciphers1, RLWECipher[] ciphers2) => NOT(AND(ciphers1, ciphers2));
 
-    public RLWECipher[] NOR(RLWECipher[] cipher1, RLWECipher[] cipher2) => AND(NOT(cipher1), NOT(cipher2));
+    public RLWECipher[] NOR(RLWECipher[] ciphers1, RLWECipher[] ciphers2) => AND(NOT(ciphers1), NOT(ciphers2));
 
-    public RLWECipher[] OR(RLWECipher[] cipher1, RLWECipher[] cipher2) => NOT(NOR(cipher1, cipher2));
+    public RLWECipher[] OR(RLWECipher[] ciphers1, RLWECipher[] ciphers2) => NOT(NOR(ciphers1, ciphers2));
 
-    public RLWECipher[] XOR(RLWECipher[] cipher1, RLWECipher[] cipher2)
+    public RLWECipher[] XOR(RLWECipher[] ciphers1, RLWECipher[] ciphers2)
     {
-        var c1_and_nc2 = AND(cipher1, NOT(cipher2));
-        var nc1_and_c2 = AND(NOT(cipher1), cipher2);
-        return c1_and_nc2.Zip(nc1_and_c2).Select(e => e.First + e.Second).ToArray();
+        return ciphers1.Zip(ciphers2).Select(e => XOR(e.First, e.Second)).ToArray();
     }
 
     public RLWECipher OP(string name, RLWECipher cipher1, RLWECipher cipher2)
@@ -235,18 +234,19 @@ public partial class RLWE
             throw new($"op:{name} not found");
     }
 
-    public RLWECipher[] OP(string name, RLWECipher[] cipher1, RLWECipher[] cipher2)
+    public RLWECipher[] OP(string name, RLWECipher[] ciphers1, RLWECipher[] ciphers2)
     {
-        return cipher1.Zip(cipher2).Select(e => OP(name, e.First, e.Second)).ToArray();
+        return ciphers1.Zip(ciphers2).Select(e => OP(name, e.First, e.Second)).ToArray();
     }
 
-    public RLWECipher[] ADD(RLWECipher[] cipher1, RLWECipher[] cipher2)
+    public RLWECipher[] ADD(RLWECipher[] ciphers1, RLWECipher[] ciphers2)
     {
         var carry = PK.Zero;
         var sum = new List<RLWECipher>();
-        for (int i = 0; i < cipher1.Length; i++)
+        for (int i = 0; i < ciphers1.Length; i++)
         {
-            var (xor, and, or) = (XOR(cipher1[i], cipher2[i]), AND(cipher1[i], cipher2[i]), OR(cipher1[i], cipher2[i]));
+            var (xor, and, or) = (XOR(ciphers1[i], ciphers2[i]), AND(ciphers1[i], ciphers2[i]),
+                OR(ciphers1[i], ciphers2[i]));
             sum.Add(XOR(xor, carry));
             carry = OR(and, AND(or, carry));
         }
