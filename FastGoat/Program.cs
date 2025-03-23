bd7eb022d80d0c306678c84fcfd45edd15a4dd8f
aidevnn <qadassi.ilyas@gmail.com>
2025-03-23 09:36:39 +0300

    Test all examples

--- FastGoat/Program.cs
+++ FastGoat/Program.cs
@@ -7,6 +7,7 @@ using FastGoat.Structures.VecSpace;
 using FastGoat.UserGroup;
 using FastGoat.UserGroup.Integers;
 using FastGoat.UserGroup.LWE;
+using FastGoat.UserGroup.Matrix;
 using FastGoat.UserGroup.Padic;
 using FastGoat.UserGroup.Polynoms;
 using Rq = FastGoat.Structures.VecSpace.KPoly<FastGoat.UserGroup.Integers.Rational>;
@@ -22,9 +23,21 @@ Console.WriteLine("Hello World");
 Ring.DisplayPolynomial = MonomDisplay.StarCaret;
 
 {
-    var (sk, tk) = (0, 1000);
+    var (sk, tk) = (5, 5);
+    // 0->4   done ~10min 0 error
+    // 5->9   done ~25min 0 error
+    // 10->14 done ~15min 0 error
+    // 15->19 done ~12min 0 error
+    // 20->24 done ~12min 2 planned errors
+    // 25->29 done ~2min  0 error
+    // 30->34 done ~20min 0 error
+    // 35->39 done ~16min 0 error
+    // 40->44 done ~40min 0 error
+    // 45->49 done ~8min  0 error
+    // 50->54 done ~2min  0 error
+    // 55->57 done ~8min  0 error
     var examplesList = Assembly.GetExecutingAssembly().GetTypes()
-        .Where(t => t.Namespace == "FastGoat.Examples" && t.IsPublic && t.IsClass)
+        .Where(t => t.Namespace == "FastGoat.Examples" && t.IsPublic)
         .OrderBy(t => t.FullName)
         .ToList();
 
@@ -32,7 +45,7 @@ Ring.DisplayPolynomial = MonomDisplay.StarCaret;
     {
         if (examplesClass.FullName != null && examplesClass.FullName.Contains("CocyclesDFS"))
             continue;
-        
+
         Console.WriteLine($"@@@@@@[{idxType}] {examplesClass.FullName} @@@@@@");
         Console.WriteLine();
 
@@ -42,7 +55,7 @@ Ring.DisplayPolynomial = MonomDisplay.StarCaret;
                         m.ReturnType.Equals(typeof(void)))
             .OrderBy(m => m.Name)
             .ToArray();
-    
+
         foreach (var (idxMeth, meth) in staticVoidMeths.Index())
         {
             Console.WriteLine($"@@@@@@[{idxType}/{idxMeth}] {examplesClass.FullName}.{meth.Name}()");
@@ -58,4 +71,6 @@ Ring.DisplayPolynomial = MonomDisplay.StarCaret;
             }
         }
     }
+
+    Console.Beep();
 }
