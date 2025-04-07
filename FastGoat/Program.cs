effde46e85a43200ce329466607ee6fb6c54e78c
aidevnn <qadassi.ilyas@gmail.com>
2025-04-07 15:07:26 +0300

    ECDLP

--- FastGoat/Program.cs
+++ FastGoat/Program.cs
@@ -1,8 +1,11 @@
 ﻿using FastGoat.Commons;
+using FastGoat.Examples;
 using FastGoat.Structures;
 using FastGoat.Structures.CartesianProduct;
 using FastGoat.Structures.GenericGroup;
+using FastGoat.Structures.VecSpace;
 using FastGoat.UserGroup;
+using FastGoat.UserGroup.EllCurve;
 using FastGoat.UserGroup.Integers;
 using static FastGoat.Commons.IntExt;
 
@@ -42,7 +45,7 @@ void BenchBSGS<T>(ConcreteGroup<T> g) where T : struct, IElt<T>
     Console.WriteLine("Elements Orders : {0} BSGS",
         eltsOrdBSGS.Values.GroupBy(a => a).ToDictionary(a => a.Key, a => a.Count()).AscendingByKey()
             .GlueMap(fmt: "[{0}]:{1}"));
-    
+
     if (g.ElementsOrders.Any(e => eltsOrdBSGS[e.Key] != e.Value))
         throw new();
 
@@ -53,10 +56,11 @@ void BenchBSGS<T>(ConcreteGroup<T> g) where T : struct, IElt<T>
     Console.WriteLine();
 }
 
+void testsBSGS()
 {
     BSGSonAbelianGroup();
     BSGSonAbelianGroup();
-    
+
     BenchBSGS(new Cn(908));
     BenchBSGS(Product.Generate(new Cn(23), new Cn(61)));
     BenchBSGS(Product.Generate(FG.Dihedral(4), FG.Dihedral(8), FG.Dihedral(10)));
@@ -83,4 +87,73 @@ void BenchBSGS<T>(ConcreteGroup<T> g) where T : struct, IElt<T>
 // 
 // # Old  Avg Time:2278 ms Dev:24.244
 // # BSGS Avg Time:458 ms Dev:7.414
+// 
+
+// Elliptic Curve Discrete Logarithm Problem
+void ECDLP(int p, (int x, int y) P0, (int x, int y) Q0, int[] curve)
+{
+    var (Poly, trans, rev) = EllipticCurves.MinimizedFormZnInt(p, curve);
+    var (A, B) = (Poly[1], Poly[0]);
+    var E = new EllGroup<ZnInt>(A, B);
+    var nb = EllipticCurves.SchoofEllPtsCount(A.K, B.K, p);
+    Console.WriteLine($"|{E}| = {nb}");
+
+    var P = new EllPt<ZnInt>(new(p, P0.x), new(p, P0.y));
+    var Q = new EllPt<ZnInt>(new(p, Q0.x), new(p, Q0.y));
+
+    var (P1, Q1) = (trans(P), trans(Q));
+    var k = Group.BSGS(E, P1, Q1, nb);
+    var Q2 = E.Times(P1, k);
+    Console.WriteLine($"P1={P1} Q1={Q1} {k}xP1=Q1");
+    Console.WriteLine($"{k} x {P} = {Q}");
+    Console.WriteLine();
+    if (!Q2.Equals(Q1))
+        throw new();
+}
+
+{
+    Ring.DisplayPolynomial = MonomDisplay.StarCaret;
+    Logger.Level = LogLevel.Level1;
+    
+    // C : y^2 = x^3 + x^2 + x + 1 p = 97, P = (7, 20), Q = (17, 46)
+    // sage: E=EllipticCurve(GF(97),[0,1,0,1,1]);P=E([7, 20]);Q=Ea([17, 46]);Q.log(P)
+    // pari: E=ellinit([0,1,0,1,1],97);elllog(E,[17, 46],[7, 20])
+    ECDLP(97, (7, 20), (17, 46), [0, 1, 0, 1, 1]);
+    
+    // (a) C : y^2 = x^3 + x^2 + x + 3, p = 103, P = (7, 14), Q = (8, 22).
+    ECDLP(103, (7, 14), (8, 22), [0, 1, 0, 1, 3]);
+
+    // (b) C : y^2 = x^3 − 2x^2 + 5x + 6, p = 149, P = (11, 16), Q = (110, 46).
+    ECDLP(149, (11, 16), (110, 46), [0, -2, 0, 5, 6]);
+
+    // (c) C : y^2 = x^3 + x^2 + x + 2, p = 10037, P = (8, 7358), Q = (2057, 5437).
+    ECDLP(10037, (8, 7358), (2057, 5437), [0, 1, 0, 1, 2]);
+}
+// Elliptic curve      y^2 = x^3 + x^2 + x + 1 in Z/97Z
+// Simplified form     y^2 = x^3 + 33*x + 69
+// 
+// |Ell[33,69](Z/97Z)| = 100
+// P1=(72,77) Q1=(82,51) 47xP1=Q1
+// 47 x ( 7,20) = (17,46)
+// 
+// Elliptic curve      y^2 = x^3 + x^2 + x +   3 in Z/103Z
+// Simplified form     y^2 = x^3 +  35*x +  18
+// 
+// |Ell[ 35, 18](Z/103Z)| = 109
+// P1=( 76, 89) Q1=( 77, 81) 76xP1=Q1
+// 76 x (  7, 14) = (  8, 22)
+// 
+// Elliptic curve      y^2 = x^3 + 147*x^2 +   5*x +   6 in Z/149Z
+// Simplified form     y^2 = x^3 + 103*x +  86
+// 
+// |Ell[103, 86](Z/149Z)| = 169
+// P1=( 60,133) Q1=( 10,103) 87xP1=Q1
+// 87 x ( 11, 16) = (110, 46)
+// 
+// Elliptic curve      y^2 = x^3 + x^2 + x +     2 in Z/10037Z
+// Simplified form     y^2 = x^3 +  6692*x +  9667
+// 
+// |Ell[ 6692, 9667](Z/10037Z)| = 10151
+// P1=( 3354, 2679) Q1=( 5403, 4600) 1277xP1=Q1
+// 1277 x (    8, 7358) = ( 2057, 5437)
 // 
