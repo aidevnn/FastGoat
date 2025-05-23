262f95c9c21e20ffc3d492f52cb66a44bb71b574
aidevnn <qadassi.ilyas@gmail.com>
2025-05-23 11:10:01 +0300

    Cleaning worktable

--- FastGoat/Program.cs
+++ FastGoat/Program.cs
@@ -5,7 +5,6 @@ using FastGoat.UserGroup;
 using FastGoat.UserGroup.Integers;
 using static FastGoat.Commons.IntExt;
 
-
 //////////////////////////////////
 //                              //
 //    Work Table for crafting   //
@@ -14,27 +13,3 @@ using static FastGoat.Commons.IntExt;
 
 Console.WriteLine("Hello World");
 Ring.DisplayPolynomial = MonomDisplay.StarCaret;
-
-{
-    var (p, n) = (2, 4);
-    var q = BigInteger.Pow(p, n);
-    var a = FG.FqX(q, 'a');
-    var g = NumberTheory.PrimitiveRoot(a);
-    foreach (var k in DividorsBigInt(q - 1).Select(k => (int)k).Order().ToArray())
-    {
-        for (int i = 1; i < q; i++)
-        {
-            var e = g.Pow(i);
-            if (!e.FastPow((q - 1) / k).IsOne())
-                continue;
-            var kthRoot = NumberTheory.NthRoot(e, k, g);
-            var et = kthRoot.Pow(k);
-            var test = et.Equals(e);
-            Console.WriteLine(new { i, k, e, kthRoot });
-            if (!test)
-                throw new();
-        }
-
-        Console.WriteLine();
-    }
-}
