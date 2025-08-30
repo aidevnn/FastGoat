f4c460ecdc31f96fde14161f3da4ed9f884f74b8
aidevnn <qadassi.ilyas@gmail.com>
2025-08-30 13:41:13 +0300

    Crafting Rho prime factorization

--- Craft/Program.cs
+++ Craft/Program.cs
@@ -19,13 +19,64 @@ using static FastGoat.Commons.IntExt;
 //////////////////////////////////
 
 Console.WriteLine("Hello World");
-RecomputeAllPrimesUpTo(32000000);
+RecomputeAllPrimesUpTo(5000000);
+
+BigInteger RhoF(BigInteger x, BigInteger N) => BigInteger.Remainder(x * x + 1, N);
+
+BigInteger RhoFactor(BigInteger N)
+{
+    BigInteger x1 = 2;
+    var x2 = RhoF(x1, N);
+    BigInteger d;
+    var set = new HashSet<(BigInteger, BigInteger)>();
+    set.Add((x1, x2));
+    do
+    {
+        x1 = RhoF(x1, N);
+        x2 = RhoF(RhoF(x2, N), N);
+        d = GcdBigInt(AmodPbigint(x1 - x2, N), N);
+        Console.WriteLine($"    d={d} x1={x1} x2={x2} N={N}");
+        if (!set.Add((x1, x2)))
+            break;
+    } while (!(d > 1 && d < N));
+
+    set.Clear();
+    if (d == 1)
+        d = N;
+    
+    return d;
+}
+
+IEnumerable<BigInteger> RhoPrime(BigInteger N)
+{
+    var N0 = N;
+    while (N0 != 1)
+    {
+        var d = RhoFactor(N0);
+        yield return d;
+        N0 /= d;
+    }
+}
 
 {
-    Console.WriteLine(NextPrimes(2, 20).Glue(", "));
-    Console.WriteLine(NextPrimes(1000, 10).Glue(", "));
-    Console.WriteLine(NextPrime(BigInteger.Pow(10, 6)));
-    Console.WriteLine(NextPrime(BigInteger.Pow(10, 10)));
-    Console.WriteLine(NextPrime(BigInteger.Pow(10, 14)));
-    Console.WriteLine(NextPrime(BigInteger.Pow(10, 15)));
+    for (BigInteger i = 3; i < 100; i++)
+    {
+        var facts = RhoPrime(i).Order().ToArray();
+        Console.WriteLine($"i = {i} = {facts.Glue(" * ")} = {PrimesDecompositionBigInt(i).Glue(" * ")}");
+        Console.WriteLine();
+    }
+
+    {
+        var i = 144493;
+        var facts = RhoPrime(i).Order().ToArray();
+        Console.WriteLine($"i = {i} = {facts.Glue(" * ")} = {PrimesDecompositionBigInt(i).Glue(" * ")}");
+        Console.WriteLine();
+    }
+    
+    {
+        BigInteger i = BigInteger.Pow(10, 6) + 3;
+        var facts = RhoPrime(i).Order().ToArray();
+        Console.WriteLine($"i = {i} = {facts.Glue(" * ")} = {PrimesDecompositionBigInt(i).Glue(" * ")}");
+        Console.WriteLine();
+    }
 }
