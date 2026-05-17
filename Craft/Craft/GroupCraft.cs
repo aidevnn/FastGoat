f94a85ef4269c92aff346e27025775a61489d705
aidevnn <qadassi.ilyas@gmail.com>
2026-05-17 06:57:13 +0300

    Crafting Factor Direct

--- Craft/Craft/GroupCraft.cs
+++ Craft/Craft/GroupCraft.cs
@@ -1,3 +1,4 @@
+using FastGoat.Commons;
 using FastGoat.Structures;
 using FastGoat.Structures.GenericGroup;
 using FastGoat.Structures.Subgroups;
@@ -46,7 +47,7 @@ public static class GroupCraft
 
         if (g.Count() != tmpElements.Count)
             return [];
-        
+
         return newGens.ToArray();
     }
 
@@ -67,7 +68,7 @@ public static class GroupCraft
         return RecreateGenerators(g, g.ToArray(), comp);
     }
 
-    public static HashSet<T> GenerateElementsLimited<T>(IGroup<T> bg, T[] generators, int limits) 
+    public static HashSet<T> GenerateElementsLimited<T>(IGroup<T> bg, T[] generators, int limits)
         where T : struct, IElt<T>
     {
         var elements = new HashSet<T>([bg.Neutral()]);
@@ -89,9 +90,10 @@ public static class GroupCraft
 
         if (limits < generatedElements.Count)
             return [];
-        
+
         return generatedElements;
     }
+
     public static HashSet<T> GenerateElementsLimited<T>(IGroup<T> bg, HashSet<T> elements, T[] generators, int limits)
         where T : struct, IElt<T>
     {
@@ -108,7 +110,7 @@ public static class GroupCraft
                 var e3 = bg.Op(e2, e1);
                 if (generatedElements.Add(e3))
                     q.Enqueue(e3);
-                
+
                 if (limits < generatedElements.Count)
                     return [];
             }
@@ -116,7 +118,69 @@ public static class GroupCraft
 
         if (limits < generatedElements.Count)
             return [];
-        
+
         return generatedElements;
+    }
+
+    public static Dictionary<ConcreteGroup<T>, List<GroupSubset<T>>> AllFactors<T>(ConcreteGroup<T> g,
+        ConcreteGroup<T> h)
+        where T : struct, IElt<T>
+    {
+        var og = g.Count();
+        var oh = h.Count();
+        var ok = og / oh;
+        var h0 = h.Except([h.Neutral()]).ToHashSet();
+        var allSubGrs = new HashSet<GroupSubset<T>>(og * og);
+        var table = new Dictionary<ConcreteGroup<T>, List<GroupSubset<T>>>(og * og, new GroupSetEquality<T>());
+        var tablePCycles = new Dictionary<GroupSubset<T>, List<GroupSubset<T>>>(og * og);
+
+        foreach (var e0 in g.Except(h).Where(e => ok % g.ElementsOrders[e] == 0))
+        {
+            var cyc = Group.Generate(g, e0);
+            var oc = cyc.Count();
+            var setCyc = cyc.ToSet();
+            if (!allSubGrs.Contains(setCyc))
+            {
+                var conjs = table[cyc] = GroupCraft.SubGroupsConjugates(g, setCyc);
+                allSubGrs.UnionWith(conjs);
+                if (IntExt.PrimesDec(oc).Count == 1)
+                {
+                    foreach (var c in conjs)
+                        tablePCycles[c] = conjs.ToList();
+                }
+            }
+        }
+
+        var sgsRem = table.Keys.Select(c => c.ToSet()).ToHashSet();
+        var cycRem = tablePCycles.Values.SelectMany(e => e).ToHashSet();
+        while (sgsRem.Count != 0)
+        {
+            var sgs = sgsRem.OrderBy(sg => sg.Count()).ToArray();
+            var cyc = cycRem.OrderBy(sg => sg.Count()).ToArray();
+            sgsRem.Clear();
+            cycRem.Clear();
+            foreach (var (sg0, sg1) in cyc.Grid2D(sgs))
+            {
+                if (sg0.SuperSetOf(sg1) || sg0.SubSetOf(sg1) || sg0.Overlaps(h0) || sg1.Overlaps(h0))
+                    continue;
+
+                var gens = sg0.Generators.Union(sg1.Generators).ToArray();
+                var elts = GroupCraft.GenerateElementsLimited(g, sg1.Concat(sg0).ToHashSet(), gens, ok);
+                if (elts.Count == 0 || ok % elts.Count != 0 || elts.Overlaps(h0))
+                    continue;
+
+                var set = new GroupSubset<T>(gens.ToHashSet(), elts);
+                if (!allSubGrs.Contains(set))
+                {
+                    var sg2 = Group.Generate("K", g, gens);
+                    var conjsSg2 = table[sg2] = GroupCraft.SubGroupsConjugates(g, set);
+                    allSubGrs.UnionWith(conjsSg2);
+                    sgsRem.Add(sg2.ToSet());
+                    cycRem.UnionWith(tablePCycles[sg0]);
+                }
+            }
+        }
+
+        return table.Where(e => e.Key.Count() == ok && !e.Key.ToSet().Overlaps(h0)).ToDictionary();
     }
 }
--- Craft/Program.cs
+++ Craft/Program.cs
@@ -27,3 +27,59 @@ using FastGoat.UserGroup.Words.Tools;
 //////////////////////////////////
 
 Console.WriteLine("Hello World");
+
+
+Perm.Style = DisplayPerm.CyclesComma;
+
+void FactorGroup<T>(ConcreteGroup<T> G, ConcreteGroup<T> H) where T : struct, IElt<T>
+{
+    Console.WriteLine($"G:{G.ShortName} H:{H.ShortName}");
+    var allFactors = GroupCraft.AllFactors(G, H);
+    var HIsNormal = Group.IsNormalSubgroup(G, H);
+    var facts = allFactors.Keys.Where(e => HIsNormal || Group.IsNormalSubgroup(G, e)).Select(g => g.ToSet()).ToList();
+    foreach (var sol in facts.Take(1))
+    {
+        var K = Group.Generate("K", G, sol.Generators.ToArray());
+        DisplayGroup.HeadElements(K);
+        Console.WriteLine($"G:{G.ShortName} H:{H.ShortName} K:{K.ShortName} allFactors:{facts.Count}");
+        var KIsNormal = Group.IsNormalSubgroup(G, K);
+        Console.WriteLine($"H Is NormalSubgroup:{HIsNormal}");
+        Console.WriteLine($"K Is NormalSubgroup:{KIsNormal}");
+        Console.WriteLine($"Is DirectProduct   :{(HIsNormal || KIsNormal) && K.Intersect(H).Count() == 1}");
+        Console.WriteLine();
+    }
+
+    if (facts.Count == 0)
+    {
+        Console.WriteLine("No Factor Direct");
+        Console.WriteLine();
+    }
+}
+
+{
+    // < a,b | b3, a10, baba-1 >
+    // Generators of F(3x:10)2 in GL(2,31)
+    // var G = FG.WordGroup("F(3x:10)2", "b3, a10, baba-1");
+    // < a,b | a8, b2, a2ba2b, aba-1baba-1b > 
+    // Generators of MM16 x: C2 in GL(4,3)
+    // var G = FG.WordGroup("MM16 x: C2", "a8, b2, a2ba2b, aba-1baba-1b");
+    // < a,b,c | b2, c2, abab, a2bcbc, a3ca-1c >
+    // Generators of D16 x: C2 in GL(4,5)
+    var G = FG.WordGroup("D16 x: C2", "b2, c2, abab, a2bcbc, a3ca-1c");
+    // < a,b,c | a4, b4, c2, abab-1, caca-1, cbcb-1 >
+    // Generators of C2 x M(4x:4)3 in GL(4,5)
+    // var G = FG.WordGroup("C2 x M(4x:4)3", "a4, b4, c2, abab-1, caca-1, cbcb-1");
+
+    DisplayGroup.HeadElements(G);
+    var subs = G.AllSubgroups();
+    subs.Naming();
+    foreach (var e in subs.Where(e => !e.IsTrivial && e.Order != G.Count()))
+    {
+        var H = e.Representative;
+        Console.WriteLine($"############### H:{H.ShortName,-40} gensH:{H.GetGenerators().ToXSet()}");
+        DisplayGroup.HeadElements(H);
+        FactorGroup(G, H);
+        Console.WriteLine("############### END");
+        Console.WriteLine();
+    }
+}
