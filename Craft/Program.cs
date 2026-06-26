09119d54d895bf1e477e55bf27c86c1defb2bbb9
aidevnn <qadassi.ilyas@gmail.com>
2026-06-26 19:44:06 +0300

    Cleaning worktable

--- Craft/Program.cs
+++ Craft/Program.cs
@@ -30,34 +30,3 @@ using FastGoat.UserGroup.Words.Tools;
 //////////////////////////////////
 
 Console.WriteLine("Hello World");
-
-Perm.Style = DisplayPerm.CyclesComma;
-
-string GapExport(Perm[] gens)
-{
-    var old = Perm.Style;
-    Perm.Style = DisplayPerm.Gap;
-    var export = $"Group([{gens.Glue(", ")}]);";
-    Perm.Style = old;
-    return export;
-}
-
-{
-    for (int m = 3; m <= 64; m++)
-    {
-        var (dicm, autDicm) = FG.AutomorphismDicyclicPg(m);
-        DisplayGroup.HeadOrders(dicm);
-        DisplayGroup.HeadOrdersGenerators(autDicm); // for m>=3, Aut[Dic[m]] ~ Aut[D4m] ~ Hol[C2m]
-        if (m >= 3)
-        {
-            var holC2m = UGCraft.HolCn(FG.Cycles(2 * m)).ToHashSet();
-            if (!autDicm.SetEquals(holC2m))
-                throw new();
-        }
-
-        Console.WriteLine($"autDic{m}:={GapExport(autDicm.GetGenerators().ToArray())}");
-        if (m < 32)
-            Console.WriteLine($"IdGroup(autDic{m});IdGroup(AutomorphismGroup(DicyclicGroup({4 * m})));");
-        Console.WriteLine();
-    }
-}
